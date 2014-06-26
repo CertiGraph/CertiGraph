2a14d36802da945f116583ae285a755bb78d74ef
Shengyi Wang <txyyss@gmail.com>
2014-06-26 16:44:45 +0800

    Finish the definition of spatial dag.

--- spatial_graph.v
+++ spatial_graph.v
@@ -8,7 +8,7 @@ Local Open Scope pred.
 
 Instance natEqDec : EqDec nat := { t_eq_dec := eq_nat_dec }.
 
-Definition graph_node x d l r :=
+Definition trinode x d l r :=
   (mapsto x d) * (mapsto (x+1) l) * (mapsto (x+2) r).
 
 Section SpatialGraph.
@@ -18,9 +18,7 @@ Section SpatialGraph.
   
   Definition graph_fun (Q: adr -> pred world) (x: adr) :=
     (!!(x = 0) && emp) ||
-    (EX d:adr, EX l:adr, EX r:adr, !!(gamma bi x = (d, l, r)) && graph_node x d l r ⊗ ((|> Q l) ⊗ (|> Q r))).
-
-  Definition graph := HORec graph_fun.
+    (EX d:adr, EX l:adr, EX r:adr, !!(gamma bi x = (d, l, r)) && trinode x d l r ⊗ (|> Q l) ⊗ (|> Q r)).
 
   Lemma graph_fun_HOcontractive : HOcontractive graph_fun.
   Proof.
@@ -28,17 +26,44 @@ Section SpatialGraph.
     unfold graph_fun.
     apply subp_orp. apply subp_refl.
     apply subp_exp. intro d. apply subp_exp. intro l. apply subp_exp. intro r.
-    apply subp_ocon. apply subp_refl.
-    apply subp_ocon; apply eqp_subp; eapply allp_left; eauto.
+    apply subp_ocon. apply subp_ocon. apply subp_refl.
+    apply eqp_subp; eapply allp_left; eauto.
+    apply eqp_subp; eapply allp_left; eauto.
   Qed.
+
+  Definition graph := HORec graph_fun.
 
   Lemma graph_unfold:
     forall x,
       graph x = (!!(x = 0) && emp) ||
-                (EX d:adr, EX l:adr, EX r:adr, !!(gamma bi x = (d, l, r)) && graph_node x d l r ⊗
-                                                 ((|> graph l) ⊗ (|> graph r))).
+                (EX d:adr, EX l:adr, EX r:adr, !!(gamma bi x = (d, l, r)) && trinode x d l r ⊗
+                                                 (|> graph l) ⊗ (|> graph r)).
   Proof.
     intros. unfold graph at 1. rewrite HORec_fold_unfold. trivial. apply graph_fun_HOcontractive.
+  Qed.
+
+  Definition dag_fun (Q: adr -> pred world) (x: adr) :=
+    (!!(x = 0) && emp) ||
+    (EX d:adr, EX l:adr, EX r:adr, !!(gamma bi x = (d, l, r)) && trinode x d l r * ((|> Q l) ⊗ (|> Q r))).
+                                                                                                        
+  Lemma dag_fun_HOcontractive : HOcontractive dag_fun.
+  Proof.
+    apply prove_HOcontractive. intros.
+    unfold dag_fun.
+    apply subp_orp. apply subp_refl.
+    apply subp_exp. intro d. apply subp_exp. intro l. apply subp_exp. intro r.
+    apply subp_sepcon. apply subp_refl.
+    apply subp_ocon; apply eqp_subp; eapply allp_left; eauto.
+  Qed.
+
+  Definition dag := HORec dag_fun.
+
+  Lemma dag_unfold:
+    forall x,
+      dag x = (!!(x = 0) && emp) ||
+              (EX d:adr, EX l:adr, EX r:adr, !!(gamma bi x = (d, l, r)) && trinode x d l r * ((|> dag l) ⊗ (|> dag r))).
+  Proof.
+    intros. unfold dag at 1. rewrite HORec_fold_unfold. trivial. apply dag_fun_HOcontractive.
   Qed.
 
 End SpatialGraph.
