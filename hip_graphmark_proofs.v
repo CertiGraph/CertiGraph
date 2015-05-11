66d4555b5b85a5d5964dfa3aadcd2369ceed94ba
Shengyi Wang <txyyss@gmail.com>
2015-05-11 18:37:23 +0800

    All lemmas in hip_graphmark_proofs.v are proved now.

--- hip_graphmark_proofs.v
+++ hip_graphmark_proofs.v
@@ -42,44 +42,115 @@ Module GraphMark <: Mgraphmark.
   Definition update : A -> node -> nat -> node -> node -> A -> formula :=
     fun g1 x d l r g2 w => exists (Hn : x <> 0) (Hi : in_math (bm_ma_g g1) x l r), update_graph g1 x d l r Hi Hn = g2.
 
+  Lemma update_is_mark1: forall l r w (G G1 : A) x, @graph.valid adr nat natEqDec (b_pg_g G) x ->
+                                                    @edge_func adr nat natEqDec (b_pg_g G) x = l :: r :: nil ->
+                                                    update G x 1 l r G1 w ->
+                                                    mark1 adr nat natEqDec (fun d : nat => d = 1) (b_pg_g G) x (b_pg_g G1).
+  Proof.
+    intros. destruct H1 as [Hi [Hn ?]]. rewrite <- H1. split. intro z. split. destruct (t_eq_dec z x); split; intro. hnf.
+    left; auto. hnf in H2. subst; auto. left; auto. hnf in H2. destruct H2. auto. exfalso; auto. simpl. unfold change_edge_func.
+    destruct (t_eq_dec z x). subst. rewrite H0. auto. auto. split. auto. split. hnf. simpl. unfold change_node_label.
+    destruct (@t_eq_dec adr natEqDec x x). auto. exfalso; auto. intros. simpl. unfold change_node_label.
+    destruct (t_eq_dec n' x). exfalso; auto. auto.
+  Qed.
+
+  Lemma node_is_two: forall (G : A) x l r, @graph.valid adr nat natEqDec (b_pg_g G) x ->
+                                           @graph.valid adr nat natEqDec (b_pg_g G) l ->
+                                           @graph.valid adr nat natEqDec (b_pg_g G) r ->
+                                           @edge_func adr nat natEqDec (b_pg_g G) x = l :: r :: nil ->
+                                           node_connected_two adr nat natEqDec (b_pg_g G) x l r.
+  Proof.
+    intros. split. do 2 (split; auto). rewrite H2. apply in_eq. split. do 2 (split; auto). rewrite H2. apply in_cons, in_eq.
+    intros. destruct H3 as [? [? ?]]. rewrite H2 in H5. apply in_inv in H5; destruct H5. left; auto. apply in_inv in H5.
+    destruct H5. right; auto. inversion H5.
+  Qed.
+
+  Lemma marked_node_marked: forall G1 n w G2 x, @node_label adr nat natEqDec (b_pg_g G1) x = 1 ->
+                                                mark G1 n G2 w ->
+                                                @node_label adr nat natEqDec (b_pg_g G2) x = 1.
+  Proof.
+    intros. destruct H0 as [? [? ?]]. LEM ((b_pg_g G1) |= n ~o~> x satisfying (unmarked nat (fun d : nat => d = 1))).
+    specialize (H1 _ H3). hnf in H1. auto. specialize (H2 _ H3). rewrite <- H2. auto.
+  Qed.
+
+  Lemma three_identity_same:
+    forall (G G1 G2 G3 : A) x l r, (b_pg_g G) ~=~ (b_pg_g G1) ->
+                                   (b_pg_g G1) ~=~ (b_pg_g G2) ->
+                                   (b_pg_g G2) ~=~ (b_pg_g G3) ->
+                                   @graph.valid adr nat natEqDec (b_pg_g G) x ->
+                                   @graph.valid adr nat natEqDec (b_pg_g G) l ->
+                                   @graph.valid adr nat natEqDec (b_pg_g G) r ->
+                                   @edge_func adr nat natEqDec (b_pg_g G) x = l :: r :: nil ->
+                                   @graph.valid adr nat natEqDec (b_pg_g G3) x /\
+                                   @graph.valid adr nat natEqDec (b_pg_g G3) l /\
+                                   @graph.valid adr nat natEqDec (b_pg_g G3) r /\
+                                   @edge_func adr nat natEqDec (b_pg_g G3) x = l :: r :: nil.
+  Proof.
+    intros. assert ((b_pg_g G) ~=~ (b_pg_g G3)). apply (si_trans H). apply (si_trans H0). auto. split. destruct (H6 x).
+    rewrite <- H7; auto. split. destruct (H6 l). rewrite <- H7; auto. split. destruct (H6 r). rewrite <- H7; auto.
+    destruct (H6 x). rewrite <- H8; auto.
+  Qed.
+  
   Lemma axiom_1 : forall v G1 G2 G G3 x l r, valid (imp (and (lookup G x v l r) (and (mark G r G1) (and (neq v 1) (and (mark G2 l G3) (update G1 x 1 l r G2))))) (and (mark G x G3) (lookup G3 x 1 l r))).
-  Proof. admit. Qed.
+  Proof.
+    intros; intro w; hnf; unfold and; hnf; intros. destruct H as [? [? [? [? ?]]]]. destruct H as [? [? [? [? ?]]]]. hnf in H1.
+    assert (mark1 adr nat natEqDec (fun d : nat => d = 1) (b_pg_g G1) x (b_pg_g G2)).
+    apply (update_is_mark1 l r w); auto; destruct H0 as [? _]; destruct (H0 x). tauto. rewrite <- H9. auto. split.
+    apply (mark_right_root_left _ _ _ _ _ (b_pg_g G1) (b_pg_g G2) _ _ l r); auto. hnf; simpl. rewrite H. auto.
+    apply node_is_two; auto. destruct H8 as [? [? [? ?]]]. split. hnf in H10. apply (marked_node_marked G2 l w); auto.
+    destruct H0, H2. apply (three_identity_same G G1 G2); auto.
+  Qed.
 
   Lemma axiom_2 : forall v G1 G2 G G3 x l r, valid (imp (and (lookup G x v l r) (and (mark G l G1) (and (neq v 1) (and (mark G2 r G3) (update G1 x 1 l r G2))))) (and (mark G x G3) (lookup G3 x 1 l r))).
-  Proof. admit. Qed.
+  Proof.
+    intros; intro w; hnf; unfold and; hnf; intros. destruct H as [? [? [? [? ?]]]]. destruct H as [? [? [? [? ?]]]]. hnf in H1.
+    assert (mark1 adr nat natEqDec (fun d : nat => d = 1) (b_pg_g G1) x (b_pg_g G2)).
+    apply (update_is_mark1 l r w); auto; destruct H0 as [? _]; destruct (H0 x). tauto. rewrite <- H9. auto. split.
+    apply (mark_left_root_right _ _ _ _ _ (b_pg_g G1) (b_pg_g G2) _ _ l r); auto. hnf; simpl. rewrite H. auto.
+    apply node_is_two; auto. destruct H8 as [? [? [? ?]]]. split. hnf in H10. apply (marked_node_marked G2 r w); auto.
+    destruct H0, H2. apply (three_identity_same G G1 G2); auto.
+  Qed.
 
   Lemma axiom_3 : forall v G1 G2 G G3 x l r, valid (imp (and (lookup G x v l r) (and (mark G r G1) (and (neq v 1) (and (mark G1 l G2) (update G2 x 1 l r G3))))) (and (mark G x G3) (lookup G3 x 1 l r))).
-  Proof. admit. Qed.
+  Proof.
+    intros; intro w; hnf; unfold and; hnf; intros. destruct H as [? [? [? [? ?]]]]. destruct H as [? [? [? [? ?]]]]. hnf in H1.
+    assert ((b_pg_g G) ~=~ (b_pg_g G2)). destruct H0, H2. apply (si_trans H0); auto.
+    assert (mark1 adr nat natEqDec (fun d : nat => d = 1) (b_pg_g G2) x (b_pg_g G3)). apply (update_is_mark1 l r w); auto.
+    destruct (H8 x). tauto. destruct (H8 x). rewrite <- H10. auto. split.
+    apply (mark_right_left_root _ _ _ _ _ (b_pg_g G1) (b_pg_g G2) _ _ l r); auto. hnf; simpl. rewrite H. auto.
+    apply node_is_two; auto. destruct H9 as [? [? [? ?]]]. split. hnf in H11. auto. destruct H0, H2.
+    apply (three_identity_same G G1 G2); auto.
+  Qed.
 
   Lemma axiom_4 : forall v G1 G2 G G3 x l r, valid (imp (and (lookup G x v l r) (and (mark G l G1) (and (neq v 1) (and (mark G1 r G2) (update G2 x 1 l r G3))))) (and (mark G x G3) (lookup G3 x 1 l r))).
-  Proof. admit. Qed.
+  Proof.
+    intros; intro w; hnf; unfold and; hnf; intros. destruct H as [? [? [? [? ?]]]]. destruct H as [? [? [? [? ?]]]]. hnf in H1.
+    assert ((b_pg_g G) ~=~ (b_pg_g G2)). destruct H0, H2. apply (si_trans H0); auto.
+    assert (mark1 adr nat natEqDec (fun d : nat => d = 1) (b_pg_g G2) x (b_pg_g G3)). apply (update_is_mark1 l r w); auto.
+    destruct (H8 x). tauto. destruct (H8 x). rewrite <- H10. auto. split.
+    apply (mark_left_right_root _ _ _ _ _ (b_pg_g G1) (b_pg_g G2) _ _ l r); auto. hnf; simpl. rewrite H. auto.
+    apply node_is_two; auto. destruct H9 as [? [? [? ?]]]. split. hnf in H11. auto. destruct H0, H2.
+    apply (three_identity_same G G1 G2); auto.
+  Qed.
 
   Lemma axiom_5 : forall v G1 G2 G G3 x l r, valid (imp (and (lookup G x v l r) (and (update G x 1 l r G1) (and (neq v 1) (and (mark G1 r G2) (mark G2 l G3))))) (and (mark G x G3) (lookup G3 x 1 l r))).
-  Proof. admit. Qed.
+  Proof.
+    intros; intro w; hnf; unfold and; hnf; intros. destruct H as [? [? [? [? ?]]]]. destruct H as [? [? [? [? ?]]]]. hnf in H1.
+    assert (mark1 adr nat natEqDec (fun d : nat => d = 1) (b_pg_g G) x (b_pg_g G1)). apply (update_is_mark1 l r w); auto.
+    split. apply (mark_root_right_left _ _ _ _ (b_pg_g G) (b_pg_g G1) (b_pg_g G2) (b_pg_g G3) x l r). hnf; simpl. rewrite H.
+    auto. apply node_is_two; auto. auto. apply H2. apply H3. destruct H8 as [? [? [? ?]]]. split. hnf in H10.
+    apply (marked_node_marked G2 l w); auto. apply (marked_node_marked G1 r w); auto. destruct H2, H3.
+    apply (three_identity_same G G1 G2); auto.
+  Qed.
 
   Lemma axiom_6 : forall v G1 G2 G G3 x l r, valid (imp (and (lookup G x v l r) (and (update G x 1 l r G1) (and (neq v 1) (and (mark G1 l G2) (mark G2 r G3))))) (and (mark G x G3) (lookup G3 x 1 l r))).
   Proof.
     intros; intro w; hnf; unfold and; hnf; intros. destruct H as [? [? [? [? ?]]]]. destruct H as [? [? [? [? ?]]]]. hnf in H1.
-    assert (mark1 adr nat natEqDec (fun d : nat => d = 1) (b_pg_g G) x (b_pg_g G1)). destruct H0 as [Hi [Hn ?]].
-    rewrite <- H0. split. intro z. split. destruct (t_eq_dec z x); split; intro. hnf. left; auto. hnf in H8. subst. auto.
-    left; auto. hnf in H8. destruct H8. auto. exfalso; auto. simpl. unfold change_edge_func. destruct (t_eq_dec z x).
-    subst. rewrite H7. auto. auto. split. auto. split. hnf. simpl. unfold change_node_label.
-    destruct (@t_eq_dec adr natEqDec x x). auto. exfalso; auto. intros. simpl. unfold change_node_label.
-    destruct (t_eq_dec n' x). exfalso; auto. auto. split.
+    assert (mark1 adr nat natEqDec (fun d : nat => d = 1) (b_pg_g G) x (b_pg_g G1)). apply (update_is_mark1 l r w); auto. split.
     apply (mark_root_left_right _ _ _ _ (b_pg_g G) (b_pg_g G1) (b_pg_g G2) (b_pg_g G3) x l r). hnf; simpl. rewrite H. auto.
-    split. do 2 (split; auto). rewrite H7. apply in_eq. split. do 2 (split; auto). rewrite H7. apply in_cons, in_eq.
-    intros. destruct H9 as [?[? ?]]. rewrite H7 in H11. apply in_inv in H11; destruct H11. left; auto. apply in_inv in H11.
-    destruct H11. right; auto. inversion H11. auto. apply H2. apply H3.
-
-    destruct H8 as [? [? [? ?]]]. split. hnf in H10. assert (@node_label _ _ _ (b_pg_g G2) x = 1). destruct H2 as [? [? ?]].
-    specialize (H12 x). specialize (H13 x). LEM ((b_pg_g G1) |= l ~o~> x satisfying (unmarked nat (fun d : nat => d = 1))).
-    specialize (H12 H14). hnf in H12. rewrite H12; auto. specialize (H13 H14). rewrite <- H13. apply H10.
-    destruct H3 as [? [? ?]]. specialize (H13 x). specialize (H14 x).
-    LEM ((b_pg_g G2) |= r ~o~> x satisfying (unmarked nat (fun d : nat => d = 1))). specialize (H13 H15). hnf in H13. auto.
-    specialize (H14 H15). rewrite <- H14. apply H12. destruct H2 as [? _]. destruct H3 as [? _].
-    assert ((b_pg_g G) ~=~ (b_pg_g G2)). apply (si_trans H8 H2). assert ((b_pg_g G) ~=~ (b_pg_g G3)). apply (si_trans H12 H3).
-    hnf in H13. split. destruct (H13 x). rewrite <- H14; auto. split. destruct (H13 l). rewrite <- H14; auto. split.
-    destruct (H13 r). rewrite <- H14; auto. specialize (H13 x). destruct H13. rewrite <- H14. auto.
+    apply node_is_two; auto. auto. apply H2. apply H3. destruct H8 as [? [? [? ?]]]. split. hnf in H10.
+    apply (marked_node_marked G2 r w); auto. apply (marked_node_marked G1 l w); auto.
+    destruct H2 as [? _]. destruct H3 as [? _]. apply (three_identity_same G G1 G2); auto.
   Qed.
 
   Lemma axiom_7 : forall v G x G1 y l r, valid (imp (and (mark G x G1) (lookup G y v l r)) (and (subset_reach G x G1) (and (eq_notreach G x G1) (ext (fun Anon_15 => (lookup G1 y Anon_15 l r)))))).
