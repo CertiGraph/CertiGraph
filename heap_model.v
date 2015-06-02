890e690e3b9e009609b6474c0ed13558352cff59
Shengyi Wang <txyyss@gmail.com>
2015-06-02 17:38:25 +0800

    remove unnecessary definitions.

--- heap_model.v
+++ heap_model.v
@@ -10,33 +10,33 @@ Proof. constructor; intros; inv H. Qed.
 Instance psa_discrete (A: Type) :  @Pos_alg A  (Join_discrete A).
 Proof. repeat intro. inv H. Qed.
 
-Definition table (A B : Type) := list (A*B).
+(* Definition table (A B : Type) := list (A*B). *)
 
-Fixpoint table_get {A B}{H: EqDec A} (rho: table A B) (x: A) : option B :=
-  match rho with
-    | (y,v)::ys => if eq_dec x y then Some v else table_get ys x
-    | nil => None
-  end.
+(* Fixpoint table_get {A B}{H: EqDec A} (rho: table A B) (x: A) : option B := *)
+(*   match rho with *)
+(*     | (y,v)::ys => if eq_dec x y then Some v else table_get ys x *)
+(*     | nil => None *)
+(*   end. *)
 
-Definition table_set {A B}{H: EqDec A} (x: A) (v: B) (rho: table A B)  : table A B := (x,v)::rho.
+(* Definition table_set {A B}{H: EqDec A} (x: A) (v: B) (rho: table A B)  : table A B := (x,v)::rho. *)
 
-Lemma table_gss {A B}{H: EqDec A}: forall rho x (v: B), table_get (table_set x v rho) x = Some v.
-Proof.
-  intros.
-  simpl. destruct (eq_dec x x); auto. contradiction n; auto.
-Qed.
+(* Lemma table_gss {A B}{H: EqDec A}: forall rho x (v: B), table_get (table_set x v rho) x = Some v. *)
+(* Proof. *)
+(*   intros. *)
+(*   simpl. destruct (eq_dec x x); auto. contradiction n; auto. *)
+(* Qed. *)
 
-Lemma table_gso {A B}{H: EqDec A}: forall rho x y (v:B), x<>y -> table_get (table_set x v rho) y = table_get rho y.
-Proof.
-  intros.
-  simpl. destruct (eq_dec y x); auto.  contradiction H0; auto.
-Qed.
+(* Lemma table_gso {A B}{H: EqDec A}: forall rho x y (v:B), x<>y -> table_get (table_set x v rho) y = table_get rho y. *)
+(* Proof. *)
+(*   intros. *)
+(*   simpl. destruct (eq_dec y x); auto.  contradiction H0; auto. *)
+(* Qed. *)
 
 Definition var := nat.
 Definition adr := nat.
-Definition stack := table var adr.
-Definition heap := table adr adr.
-Definition state := (stack * heap)%type.
+(* Definition stack := table var adr. *)
+(* Definition heap := table adr adr. *)
+(* Definition state := (stack * heap)%type. *)
 
 Definition world := (fpm adr adr).
 
