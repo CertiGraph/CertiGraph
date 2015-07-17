e8d3734c694c6865f780dace0a881be2eff5b011
Shengyi Wang <txyyss@gmail.com>
2015-07-17 18:03:43 +0800

    Reproved that  vi is a equivalence relation.

--- data_structure/general_spatial_graph.v
+++ data_structure/general_spatial_graph.v
@@ -37,8 +37,39 @@ Definition validly_identical {V E DV DE} (g1 g2: SpatialGraph V E DV DE) : Prop 
 
 Notation "g1 '-=-' g2" := (validly_identical g1 g2) (at level 1).
 
+Lemma vi_refl {V E DV DE}: forall (g : SpatialGraph V E DV DE), g -=- g.
+Proof. intros. split; auto. apply si_refl. Qed.
+
+Lemma vi_sym {V E DV DE}: forall (g1 g2 : SpatialGraph V E DV DE), g1 -=- g2 -> g2 -=- g1.
+Proof.
+  intros. destruct H as [? [? ?]]. split; [|split]; intros.
+  + apply si_sym; auto.
+  + specialize (H0 _ H3 H2). auto.
+  + specialize (H1 _ H3 H2). auto.
+Qed.
+
+Lemma vi_trans {V E DV DE}: forall (g1 g2 g3: SpatialGraph V E DV DE), g1 -=- g2 -> g2 -=- g3 -> g1 -=- g3.
+Proof.
+  intros. destruct H as [? [? ?]]. destruct H0 as [? [? ?]].
+  split; [| split]; intros.
+  + apply si_trans with g2; auto.
+  + assert (@vvalid V E g2 v) by (destruct H; rewrite <- H; auto).
+    specialize (H1 _ H5 H7). specialize (H3 _ H7 H6). transitivity (vgamma g2 v); auto.
+  + assert (@evalid V E g2 e) by (destruct H as [_ [? _]]; rewrite <- H; auto).
+    specialize (H2 _ H5 H7). specialize (H4 _ H7 H6). transitivity (egamma g2 e); auto.
+Qed.
+
+Add Parametric Relation {V E DV DE} : (SpatialGraph V E DV DE) validly_identical
+    reflexivity proved by vi_refl
+    symmetry proved by vi_sym
+    transitivity proved by vi_trans as vi_equal.
+  
 Definition predicate_sub_spatialgraph {V E DV DE: Type} (g: SpatialGraph V E DV DE: Type) (p: V -> Prop) :=
   Build_SpatialGraph V E DV DE (predicate_subgraph g p) (vgamma g) (egamma g).
+
+Definition unreachable_sub_spatialgraph {V E DV DE: Type}
+           (g: SpatialGraph V E DV DE: Type) (S : list V) : SpatialGraph V E DV DE :=
+  predicate_sub_spatialgraph g (fun n => ~ reachable_through_set g S n).
 
 Class SpatialGraphPred (V E DV DE Pred: Type): Type := {
   vertex_at: V -> DV -> Pred;
@@ -62,6 +93,7 @@ Class SpatialGraphStrongAssum {V E DV DE Pred: Type} (SGP: SpatialGraphPred V E 
   SGP_COSL: CorableOverlapSepLog Pred;
 
   AAV: AbsAddr V DV;
+  VSELF_CONFLICT: forall x, @addr_conflict _ _ AAV x x = true;
   AAE: AbsAddr E DE;
   VP_MSL: MapstoSepLog AAV vertex_at;
   VP_sMSL: StaticMapstoSepLog AAV vertex_at;
@@ -85,8 +117,8 @@ Section SpatialGraph.
   Proof.
     repeat intro. unfold graph_cell.
     apply (@mapsto_conflict _ _ _ _ _ _ _ _ _ _ _ VP_sMSL).
-    (* unfold addr_conflict. unfold addr_eqb. destruct (addr_eq_dec x x); auto. *)
-  Abort.
+    apply VSELF_CONFLICT.
+  Qed.
   
   Definition graph (x : V) (g: Graph) : Pred :=
     EX l : list V, !!reachable_list g x l && iter_sepcon l (graph_cell g).
@@ -117,9 +149,10 @@ Section SpatialGraph.
       unfold graphs'. unfold graph. clear IHS. apply pred_ext.
       - normalize_overlap. intros. rename x into la.
         normalize_overlap. rename x into lS. normalize_overlap.
-        (* rewrite (add_andp _ _ (iter_sepcon_unique_nodup la (sepcon_unique_graph_cell g))). *)
-        (* rewrite (add_andp _ _ (iter_sepcon_unique_nodup lS (sepcon_unique_graph_cell g))). *)
-        (* normalize_overlap. *)
+        rewrite (add_andp _ _ (iter_sepcon_unique_nodup la (sepcon_unique_graph_cell g))).
+        rewrite (add_andp _ _ (iter_sepcon_unique_nodup lS (sepcon_unique_graph_cell g))).
+        normalize_overlap.
+        (* rewrite (iter_sepcon_ocon t_eq_dec); auto. apply (exp_right (remove_dup t_eq_dec (la ++ lS))). *)
   Abort.
 
 End SpatialGraph.
