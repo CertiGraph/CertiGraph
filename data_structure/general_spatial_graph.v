80cd491f6af4a2561017c9778de6e95ca538a4c5
Shengyi Wang <txyyss@gmail.com>
2015-07-20 16:31:55 +0800

    Proved graphs_unfold lemma.

--- data_structure/general_spatial_graph.v
+++ data_structure/general_spatial_graph.v
@@ -39,15 +39,15 @@ Context {DE : Type}.
 
 Definition validly_identical (g1 g2: SpatialGraph V E DV DE) : Prop :=
   g1 ~=~ g2 /\
-  (forall v, @vvalid _ _ g1 v -> @vvalid _ _ g2 v -> vgamma g1 v = vgamma g2 v) /\
-  (forall e, @evalid _ _ g1 e -> @evalid _ _ g2 e -> egamma g1 e = egamma g2 e).
+  (forall v, vvalid g1 v -> vvalid g2 v -> vgamma g1 v = vgamma g2 v) /\
+  (forall e, evalid g1 e -> evalid g2 e -> egamma g1 e = egamma g2 e).
 
 Notation "g1 '-=-' g2" := (validly_identical g1 g2) (at level 1).
 
-Lemma vi_refl {V E DV DE}: forall (g : SpatialGraph V E DV DE), g -=- g.
+Lemma vi_refl: forall (g : SpatialGraph V E DV DE), g -=- g.
 Proof. intros. split; auto. apply si_refl. Qed.
 
-Lemma vi_sym {V E DV DE}: forall (g1 g2 : SpatialGraph V E DV DE), g1 -=- g2 -> g2 -=- g1.
+Lemma vi_sym: forall (g1 g2 : SpatialGraph V E DV DE), g1 -=- g2 -> g2 -=- g1.
 Proof.
   intros. destruct H as [? [? ?]]. split; [|split]; intros.
   + apply si_sym; auto.
@@ -55,27 +55,26 @@ Proof.
   + specialize (H1 _ H3 H2). auto.
 Qed.
 
-Lemma vi_trans {V E DV DE}: forall (g1 g2 g3: SpatialGraph V E DV DE), g1 -=- g2 -> g2 -=- g3 -> g1 -=- g3.
+Lemma vi_trans: forall (g1 g2 g3: SpatialGraph V E DV DE), g1 -=- g2 -> g2 -=- g3 -> g1 -=- g3.
 Proof.
   intros. destruct H as [? [? ?]]. destruct H0 as [? [? ?]].
   split; [| split]; intros.
   + apply si_trans with g2; auto.
-  + assert (@vvalid V E g2 v) by (destruct H; rewrite <- H; auto).
+  + assert (vvalid g2 v) by (destruct H; rewrite <- H; auto).
     specialize (H1 _ H5 H7). specialize (H3 _ H7 H6). transitivity (vgamma g2 v); auto.
-  + assert (@evalid V E g2 e) by (destruct H as [_ [? _]]; rewrite <- H; auto).
+  + assert (evalid g2 e) by (destruct H as [_ [? _]]; rewrite <- H; auto).
     specialize (H2 _ H5 H7). specialize (H4 _ H7 H6). transitivity (egamma g2 e); auto.
 Qed.
 
-Add Parametric Relation {V E DV DE} : (SpatialGraph V E DV DE) validly_identical
+Add Parametric Relation : (SpatialGraph V E DV DE) validly_identical
     reflexivity proved by vi_refl
     symmetry proved by vi_sym
     transitivity proved by vi_trans as vi_equal.
   
-Definition predicate_sub_spatialgraph {V E DV DE: Type} (g: SpatialGraph V E DV DE: Type) (p: V -> Prop) :=
-  Build_SpatialGraph V E DV DE (predicate_subgraph g p) (vgamma g) (egamma g).
+Definition predicate_sub_spatialgraph  (g: SpatialGraph V E DV DE: Type) (p: V -> Prop) :=
+  Build_SpatialGraph V E _ _ DV DE (predicate_subgraph g p) (vgamma g) (egamma g).
 
-Definition unreachable_sub_spatialgraph {V E DV DE: Type}
-           (g: SpatialGraph V E DV DE: Type) (S : list V) : SpatialGraph V E DV DE :=
+Definition unreachable_sub_spatialgraph (g: SpatialGraph V E DV DE: Type) (S : list V) : SpatialGraph V E DV DE :=
   predicate_sub_spatialgraph g (fun n => ~ reachable_through_set g S n).
 
 Class SpatialGraphPred (V E DV DE Pred: Type): Type := {
@@ -100,7 +99,7 @@ Class SpatialGraphStrongAssum {V E DV DE Pred: Type} (SGP: SpatialGraphPred V E 
   SGP_COSL: CorableOverlapSepLog Pred;
 
   AAV: AbsAddr V DV;
-  VSELF_CONFLICT: forall x, @addr_conflict _ _ AAV x x = true;
+  VSELF_CONFLICT: forall x y, x = y <-> @addr_conflict _ _ AAV x y = true;
   AAE: AbsAddr E DE;
   VP_MSL: MapstoSepLog AAV vertex_at;
   VP_sMSL: StaticMapstoSepLog AAV vertex_at;
@@ -112,7 +111,7 @@ Existing Instances SGA SGP_PSL SGP_OSL SGP_DSL SGP_COSL VP_MSL VP_sMSL EP_MSL EP
 
 Section SpatialGraph.
 
-  Context {V E DV DE Pred: Type}.
+  Context {Pred: Type}.
   Context {SGP: SpatialGraphPred V E DV DE Pred}.
   (* Context {SGA: SpatialGraphAssum SGP}. *)
   Context {SGSA: SpatialGraphStrongAssum SGP}.
@@ -120,12 +119,21 @@ Section SpatialGraph.
 
   Definition graph_cell (g: Graph) (v : V) : Pred := vertex_at v (vgamma g v).
 
+  Lemma precise_graph_cell: forall g v, precise (graph_cell g v).
+  Proof. intros. unfold graph_cell. apply (@mapsto_precise _ _ _ _ _ _ _ _ VP_MSL). Qed.  
+
   Lemma sepcon_unique_graph_cell: forall g, sepcon_unique (graph_cell g).
   Proof.
     repeat intro. unfold graph_cell.
     apply (@mapsto_conflict _ _ _ _ _ _ _ _ _ _ _ VP_sMSL).
-    apply VSELF_CONFLICT.
+    rewrite <- VSELF_CONFLICT; auto.
   Qed.
+
+  Lemma joinable_graph_cell : forall g, joinable (graph_cell g).
+  Proof.
+    intros. unfold joinable; intros. unfold graph_cell. apply (@disj_mapsto _ _ AAV _ _ _ _ _ _ VP_MSL _ VP_sMSL).
+    rewrite VSELF_CONFLICT in H. destruct (addr_conflict x y). exfalso; apply H; auto. auto.
+  Qed.  
   
   Definition graph (x : V) (g: Graph) : Pred :=
     EX l : list V, !!reachable_list g x l && iter_sepcon l (graph_cell g).
@@ -140,9 +148,28 @@ Section SpatialGraph.
     EX l: list V, !!reachable_set_list pg S l &&
                     iter_sepcon l (graph_cell g).
 
-  Lemma graphs_unfold: forall S g, graphs S g = graphs' S g.
+  Definition single_reachable_contructable (S : list V) (g : Graph) : Prop :=
+    forall s, In s S -> exists l, reachable_list g s l /\ NoDup l.
+
+  Lemma single_reachable_contructable_cons: forall a S g,
+      single_reachable_contructable (a :: S) g -> single_reachable_contructable S g.
   Proof.
-    induction S; intros.
+    intros. unfold single_reachable_contructable in *. intros.
+    assert (In s (a :: S)) by (apply in_cons; auto).
+    specialize (H _ H1). auto.
+  Qed.
+
+  Definition set_reachable_contructable (S : list V) (g : Graph) : Prop :=
+    forall s, Sublist s S -> exists l, reachable_set_list g s l /\ NoDup l.
+
+  Lemma single_set_reachble_constructable: forall S g, single_reachable_contructable S g -> set_reachable_contructable S g.
+  Proof.
+    admit.
+  Qed.
+  
+  Lemma graphs_graphs': forall S g, single_reachable_contructable S g -> graphs S g = graphs' S g.
+  Proof.
+    induction S; intros until g. intro Hs.
     + unfold graphs. unfold graphs'. apply pred_ext.
       - apply (exp_right nil). simpl. apply andp_right; auto.
         apply prop_right. intro x. split; intros.
@@ -152,14 +179,49 @@ Section SpatialGraph.
         specialize (H v). assert (In v (v :: l)) by apply in_eq.
         rewrite <- H in H0. unfold reachable_through_set in H0.
         destruct H0 as [s [? _]]. inversion H0.
-    + unfold graphs. fold graphs. rewrite IHS.
+    + intro Hs. unfold graphs. fold graphs. rewrite (IHS _ (single_reachable_contructable_cons _ _ _ Hs)).
       unfold graphs'. unfold graph. clear IHS. apply pred_ext.
       - normalize_overlap. intros. rename x into la.
         normalize_overlap. rename x into lS. normalize_overlap.
         rewrite (add_andp _ _ (iter_sepcon_unique_nodup la (sepcon_unique_graph_cell g))).
         rewrite (add_andp _ _ (iter_sepcon_unique_nodup lS (sepcon_unique_graph_cell g))).
         normalize_overlap.
-        (* rewrite (iter_sepcon_ocon t_eq_dec); auto. apply (exp_right (remove_dup t_eq_dec (la ++ lS))). *)
+        rewrite (iter_sepcon_ocon equiv_dec); auto. apply (exp_right (remove_dup equiv_dec (la ++ lS))).
+        apply andp_right.
+        * apply prop_right.
+          unfold reachable_set_list in *.
+          unfold reachable_list in *. intros.
+          rewrite <- remove_dup_in_inv.
+          rewrite reachable_through_set_eq.
+          specialize (H0 x). specialize (H x).
+          split; intro; [apply in_or_app | apply in_app_or in H3];
+          destruct H3; [left | right | left | right]; tauto.
+        * auto.
+        * apply precise_graph_cell.
+        * apply joinable_graph_cell.
+      - normalize. intro l; intros. assert (In a (a :: S)) by apply in_eq.
+        destruct (Hs _ H0) as [la [? ?]].
+        normalize_overlap. apply (exp_right la).
+        assert (Sublist S (a :: S)) by (intro s; intros; apply in_cons; auto).
+        destruct ((single_set_reachble_constructable _ _ Hs) _ H3) as [lS [? ?]].
+        normalize_overlap. apply (exp_right lS). normalize_overlap.
+        rewrite (add_andp _ _ (iter_sepcon_unique_nodup l (sepcon_unique_graph_cell g))).
+        normalize. rewrite (iter_sepcon_ocon equiv_dec); auto.
+        2: apply precise_graph_cell.
+        2: apply joinable_graph_cell.
+        rewrite iter_sepcon_permutation with (l2 := remove_dup equiv_dec (la ++ lS)); auto.
+        apply NoDup_Permutation; auto. apply remove_dup_nodup.
+        intros. rewrite <- remove_dup_in_inv. clear -H H1 H4.
+        specialize (H x). specialize (H1 x). specialize (H4 x). rewrite <- H.
+        rewrite reachable_through_set_eq. rewrite in_app_iff. tauto.
+  Qed.
+
+  Lemma subgraph_update:
+    forall (g g': Graph) (S1 S1' S2: list V),
+      Included (reachable_through_set g S1) (reachable_through_set g' S1') ->
+      (unreachable_sub_spatialgraph g S1) -=- (unreachable_sub_spatialgraph g' S1') ->
+      graphs' S1 g ⊗ graphs' S2 g |-- graphs' S1 g * (graphs' S1' g' -* graphs' S1' g' ⊗ graphs' S2 g').
+  Proof.
   Abort.
 
 End SpatialGraph.
