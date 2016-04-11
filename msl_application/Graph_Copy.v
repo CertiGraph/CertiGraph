fc98d1fe57920f68ca879bb6c5cf52b6fa66430c
Qinxiang <caoqinxiang@gmail.com>
2016-04-10 21:49:43 -0400

    Commit for a next-step modification in copy. Put (or have it derived) "copied vertices are not null" into pure facts of copy

--- msl_application/Graph_Copy.v
+++ msl_application/Graph_Copy.v
@@ -27,72 +27,39 @@ Import RamifyCoq.msl_ext.seplog.OconNotation.
 
 Local Open Scope logic.
 
+Class CompactCopySetting (V E: Type) := {
+  default_v: V;
+  default_e: E
+}.
+
 Section SpatialGraph_Copy.
 
 Context {V E: Type}.
 Context {SGBA: SpatialGraphBasicAssum V E}.
-Context {DV DE: Type}.
 Context {GV GE Pred: Type}.
 Context {SGP: SpatialGraphPred V E GV GE Pred}.
 Context {SGA: SpatialGraphAssum SGP}.
-Context {MGS: WeakMarkGraph.MarkGraphSetting DV}.
-Context {GMS: GraphMorphismSetting DV DE V E DV DE}.
+Context {SGC: SpatialGraphConstructor V E V E GV GE}.
+Context {L_SGC: Local_SpatialGraphConstructor V E V E GV GE}.
+Context {CCS: CompactCopySetting V E}.
 
-Notation Graph := (LabeledGraph V E DV DE).
-Notation SGraph := (SpatialGraph V E GV GE).
+Instance MGS: WeakMarkGraph.MarkGraphSetting V.
+Proof.
+  apply (WeakMarkGraph.Build_MarkGraphSetting _ (fun x => default_v <> x)).
+  intros; destruct_eq_dec default_v x; [right | left]; simpl; congruence.
+Defined.
+
+Global Existing Instance MGS.
+
+Instance GMS: GraphMorphismSetting V E V E V E :=
+  Build_GraphMorphismSetting _ _ _ _ _ _ (fun v => v) (fun e => e) default_v default_e.
+
+Global Existing Instance GMS.
+
+Notation Graph := (LabeledGraph V E V E).
+Notation SGraph := (SpatialGraph V E V E).
 
 Local Coercion pg_lg: LabeledGraph >-> PreGraph.
-
-Variable compute_vgamma: Graph -> V -> GV.
-Variable compute_egamma: Graph -> E -> GE.
-
-Hypothesis compute_vgamma_local: forall (G1 G2: Graph) (x: V),
-  ((predicate_partial_labeledgraph G1 (eq x)) ~=~ (predicate_partial_labeledgraph G1 (eq x)))%LabeledGraph ->
-  compute_vgamma G1 x = compute_vgamma G2 x.
-
-Hypothesis compute_egamma_local: forall (G1 G2: Graph) (e: E),
-  evalid G1 e ->
-  evalid G2 e ->
-  elabel G1 e = elabel G2 e ->
-  src G1 e = src G2 e ->
-  dst G1 e = dst G2 e ->
-  compute_egamma G1 e = compute_egamma G2 e.
-
-Definition Graph_SpatialGraph (G: Graph) : SGraph :=
-  Build_SpatialGraph _ _ _ _ _ _ G (compute_vgamma G) (compute_egamma G).
-
-Lemma GSG_VGenPreserve: forall (G: Graph) x lx gx,
-  gx = vgamma (Graph_SpatialGraph (labeledgraph_vgen G x lx)) x ->
-  (Graph_SpatialGraph (labeledgraph_vgen G x lx)) -=- (spatialgraph_vgen (Graph_SpatialGraph G) x gx).
-Proof.
-  intros. subst.
-  split; [| split].
-  + reflexivity.
-  + intros; simpl.
-    destruct_eq_dec x v.
-    - subst; auto.
-    - apply compute_vgamma_local; auto.
-      eapply si_stronger_partial_labeledgraph_simple; [| apply lg_vgen_stable].
-      hnf; unfold Ensembles.In; intros.
-      congruence.
-  + intros; simpl.
-    apply compute_egamma_local; auto.
-Qed.
-
-Lemma GSG_PartialGraphPreserve: forall (G: Graph) (p: V -> Prop),
-  (predicate_partial_spatialgraph (Graph_SpatialGraph G) p) -=-
-  (Graph_SpatialGraph (predicate_partial_labeledgraph G p)).
-Proof.
-  intros.
-  split; [| split].
-  + reflexivity.
-  + simpl; intros.
-    apply compute_vgamma_local; auto.
-    reflexivity.
-  + simpl; intros.
-    apply compute_egamma_local; auto.
-    destruct H; auto.
-Qed.
 
 Definition vcopy1 x (g1 g2: Graph) :=
   g1 ~=~ g2 /\
