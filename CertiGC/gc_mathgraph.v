2faff19e552cd7c210ba0d99f7039013502c15d9
Aquinas AquinasHobor <Aquinas AquinasHobor@live.com>
2017-02-10 03:12:43 +0800

    More work on the GC mathgraph.  Added some order theory, new copyedge function, etc.

--- CertiGC/gc_mathgraph.v
+++ CertiGC/gc_mathgraph.v
@@ -11,11 +11,15 @@ Require Import RamifyCoq.graph.reachable_computable.
 Require Import RamifyCoq.graph.MathGraph.
 Require Import RamifyCoq.graph.FiniteGraph.
 
+Require Import RamifyCoq.CertiGC.orders.
+
 Section GC_Graph.
 
 Local Coercion pg_lg : LabeledGraph >-> PreGraph.
 Local Coercion lg_gg : GeneralGraph >-> LabeledGraph.
 Local Coercion LocalFiniteGraph_FiniteGraph : FiniteGraph >-> LocalFiniteGraph.
+
+Open Local Scope ord.
 
 (* RawGraph: Vertices, Edges, Labels *)
 
@@ -23,13 +27,8 @@ Local Coercion LocalFiniteGraph_FiniteGraph : FiniteGraph >-> LocalFiniteGraph.
 Context {V : Type} {EV : EquivDec.EqDec V eq}.
 Context {null : V}.
 
-Context {le : V -> V -> Prop}.
-Local Notation "v1 <= v2" := (le v1 v2).
-Local Notation "v1 < v2" := (v1 <= v2 /\ v1 <> v2).
-Local Notation "v1 <= v2 <= v3" := (v1 <= v2 /\ v2 <= v3).
-Local Notation "v1 <= v2 <  v3" := (v1 <= v2 /\ v2 <  v3).
-Local Notation "v1 <  v2 <= v3" := (v1 <  v2 /\ v2 <= v3).
-Local Notation "v1 <  v2 <  v3" := (v1 <  v2 /\ v2 <  v3).
+Context {OLE : Ord V}. Existing Instance OLE.
+Context {CTV: ComparableTrans V}. Existing Instance CTV.
 
 (* These might be CompCert addresses, or perhaps other choices. *)
 Context {E : Type} {EE : EquivDec.EqDec E eq}.
@@ -98,19 +97,41 @@ Definition in_space (s : space) (x : V) : Prop :=
 
 Lemma in_space_bounds: forall s x,
   in_space s x ->
-  start s <= x /\ x < limit s.
+  start s <= x < limit s.
 Proof.
-  destruct s. unfold in_space, allocated, available. simpl.
-(* destruct space_order0. *)
-  admit.
-Admitted.
+  destruct s. unfold in_space, allocated, available. simpl. intros.
+  destruct H as [[? ?] | [? ?]]; split; trivial.
+  apply sord_ord_trans1 with next0. tauto.
+  rewrite <- H. tauto.
+Qed.
+
+Lemma bounds_in_space: forall s x,
+  start s <= x < limit s ->
+  in_space s x.
+Proof.
+  intros.
+  destruct H.
+  destruct s.
+  red. unfold available, allocated. simpl in *.
+  destruct space_order0.
+  case (EV x next0); unfold equiv, complement; intro.
+  subst. right.
+  auto with ord.
+  assert (x ~ start0) by (red; tauto).
+  assert (start0 ~ next0) by (red; tauto).
+  rewrite <- H3 in H4.
+  destruct H4.
+  left; split; red; tauto.
+  right. tauto.
+Qed.
 
 Lemma not_allocated_available: forall s x,
   ~(allocated s x /\ available s x).
 Proof.
   intros ? ? [[_ ?] [? _]].
-  admit.
-Admitted.
+  apply sord_antirefl with x.
+  eauto with ord.
+Qed.
 
 (* There's probably a better way to write this definition... *)
 Definition is_pointer : DecidablePred field_type :=
@@ -122,53 +143,60 @@ Definition is_pointer : DecidablePred field_type :=
                           I pointer H)
    end).
 
-
 (* Facts about size? *)
 Context {size : V -> V -> nat -> Prop}.
+Lemma size_ord {A} `{Ord A} : forall v1 v2 v3 n1 n2,
+  v1 <= v2 <= v3 ->
+  size v1 v3 (n1 + n2) <-> size v1 v2 n1 /\ size v2 v3 n2.
+Admitted.
+
+Definition allocated_size (s : space) (n : nat) : Prop :=
+  size (start s) (next s) n.
 Definition space_size (s : space) (n : nat) : Prop :=
   size (start s) (limit s) n.
+Definition free_size (s : space) (n : nat) : Prop :=
+  size (next s) (limit s) n.
+
 (*
 Definition size (s : space) : nat :=
   sub (limit s) (start s). (* Notation doesn't work for some reason *)
 *)
 
-Definition spaces_ok (g : raw_GC_graph) : Prop :=
+Definition spaces_disjoint (g : raw_GC_graph) : Prop :=
   (* Spaces are disjoint *)
-  (forall i j s1 s2,
+  forall i j s1 s2,
     List.nth_error (glabel g) i = Some s1 ->
     List.nth_error (glabel g) j = Some s2 ->
-      limit s1 <= start s2 \/
-      limit s2 <= start s1 \/
-      i = j) /\
+    forall x, 
+      in_space s1 x ->
+      in_space s2 x ->
+      i = j.
+
+Definition spaces_double (g : raw_GC_graph) : Prop :=
   (* Spaces double in size *)
-  (forall i s1 s2,
+  forall i s1 s2,
     List.nth_error (glabel g) i = Some s1 ->
     List.nth_error (glabel g) (S i) = Some s2 ->
     forall n1 n2,
     space_size s1 n1 ->
     space_size s2 n2 ->
-    n2 = n1 + n1).
+    n2 = n1 + n1.
 
-Lemma only_in_one_space : forall g,
-  spaces_ok g ->
-  forall x i j s1 s2,
+Lemma spaces_empty_intersection: forall g,
+  spaces_disjoint g ->
+  forall i j s1 s2,
     List.nth_error (glabel g) i = Some s1 ->
     List.nth_error (glabel g) j = Some s2 ->
-    in_space s1 x ->
-    in_space s2 x ->
-    s1 = s2.
+    ~(in_space s1 (start s2)).
 Proof.
-  intros.
-  destruct H as [? _].
-  specialize (H i j s1 s2 H0 H1).
+  repeat intro.
+  assert (in_space s2 (start s2)). 
+    { apply bounds_in_space. split. reflexivity.
+      destruct s2. simpl in *. 
   apply in_space_bounds in H2.
-  apply in_space_bounds in H3.
-  destruct H.
+  admit. }
+  specialize (H i j s1 s2 H0 H1 _ H2 H3). subst j.
   admit.
-  destruct H.
-  admit.
-  subst. rewrite H1 in H0. inversion H0.
-  trivial.
 Admitted.
 
 (* Between START and NEXT *)
@@ -179,7 +207,7 @@ Definition valid_in_generation (g : raw_GC_graph) : Prop :=
   forall x, vvalid g x -> exists n, in_generation (glabel g) x n.
 
 Lemma unallocated_not_valid: forall g n s,
-  spaces_ok g ->
+  spaces_disjoint g ->
   valid_in_generation g ->
   List.nth_error (glabel g) n = Some s ->
   forall x, available s x ->
@@ -187,11 +215,11 @@ Lemma unallocated_not_valid: forall g n s,
 Proof.
   repeat intro.
   specialize (H0 x H3). destruct H0 as [n' [s' [? ?]]].
-  generalize (only_in_one_space g H x n n' s s' H1 H0); intro.
+  specialize (H n n' s s' H1 H0 x).
   assert (in_space s x) by (right; trivial).
   assert (in_space s' x) by (left; trivial).
-  specialize (H5 H6 H7).
-  subst s'.
+  specialize (H H5 H6).
+  subst n'. rewrite H1 in H0. inversion H0. subst s'.
   eapply not_allocated_available; eauto.
 Qed.
 
@@ -213,12 +241,88 @@ Class GC_graph (g : raw_GC_graph) : Type := {
   fin: FiniteGraph g;
   cpa : copied_pointer_active g;
   rne : right_number_edges g fin;
-  sok : spaces_ok g;
+  ssj : spaces_disjoint g;
+  ssz : spaces_double g;
   vig : valid_in_generation g;
   acycg : acyclic_generations g
 }.
 
 Definition Graph : Type := GeneralGraph V E LV LE LG GC_graph.
+
+Definition g_lfg (g : Graph) : LocalFiniteGraph g :=
+  @fin g (sound_gg g).
+Local Coercion g_lfg : Graph >-> LocalFiniteGraph.
+
+Lemma copyEdge_contr: forall (g : Graph) x,
+  vvalid g x ->
+  mark (vlabel g x) = true ->
+  nil = (@edge_func _ _ _ _ g (g_lfg g) x) ->
+  False.
+Proof.
+  destruct g; simpl. destruct sound_gg.
+  intros.
+  generalize (rne0 x H); intro.
+  simpl in *.
+  replace (g_lfg
+             (@Build_GeneralGraph V E EV EE LV LE
+                LG GC_graph lg_gg
+                (Build_GC_graph lg_gg ma0 fin0
+                   cpa0 rne0 ssj0 ssz0 vig0
+                   acycg0))) with
+        (@LocalFiniteGraph_FiniteGraph V E
+                EV EE
+                (@pg_lg V E EV EE LV LE LG lg_gg)
+                fin0) in H1.
+  rewrite <- H1 in H2.
+  simpl in H2.
+  generalize (cpa0 x H H0); intro.
+  unfold num_edges in H2.
+  remember (fields (vlabel lg_gg x)).
+  destruct l. discriminate.
+  inversion H3. subst f.
+  simpl in H2. discriminate.
+  admit. (* Shengyi? *)
+Admitted.
+
+Definition copyEdge (g : Graph) (x : V) (Pf0 : vvalid g x) (Pf1 : mark (vlabel g x) = true) : E :=
+  match edge_func g x as l return l = edge_func g x -> E with
+   | (e :: _)%list => fun _ => e
+   | nil => fun Pf2 => match (copyEdge_contr _ _ Pf0 Pf1 Pf2) with end
+  end eq_refl.
+
+Section Cheney.
+
+Variables (G G' : Graph).
+
+Variables (PV : V -> Prop) (PE : E -> Prop) (vmap : V -> V) (emap : E -> E).
+
+Record guarded_Cheney_morphism: Prop := {
+  vvalid_Cpreserved: forall v, PV v -> (vvalid G v <-> vvalid G' (vmap v));
+  evalid_Cpreserved: forall e, PE e -> (evalid G e <-> evalid G' (emap e));
+  src_Cpreserved: forall e, PE e -> PV (src G e) -> evalid G e ->
+                   vmap (src G e) = src G' (emap e);
+  dst_Cpreserved: forall e, PE e -> PV (dst G e) -> evalid G e -> vmap (dst G e) = dst G' (emap e)
+}.
+
+
+  vvalid_preserved: forall v, PV v -> (vvalid G v <-> vvalid G' (vmap v));
+  evalid_preserved: forall e, PE e -> (evalid G e <-> evalid G' (emap e));
+
+
+  forall v, vvalid g1 v -> 
+
+Print reachable_subgraph.
+
+Print reachable_through_set.
+
+Print reachable.
+
+Print reachable_by.
+
+Definition CheneyIsomorphic (g1 g2 : Graph) 
+                            (roots : list V) : Prop :=
+  forall v, vvalid g1 v ->
+
 
 Record subspaces : Type := {
   inspace : space;
@@ -229,13 +333,12 @@ Record subspaces : Type := {
                    scan <= next inspace
 }.
 
+Definition 
+
 (*
 Definition P0 (n: nat) (g: Graph) (s: subspaces) : Prop := spaces
 *)
 
-Definition g_lfg (g : Graph) : LocalFiniteGraph g :=
-  @fin g (sound_gg g).
-Local Coercion g_lfg : Graph >-> LocalFiniteGraph.
 
 Definition P0 (n : nat) (s1 s2 : subspaces) (g : Graph) (g' : Graph) : Prop :=
   exists s, exists s',
