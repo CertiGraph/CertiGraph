fd2422ad8a524a153fbe833b18201ea1aab47b26
Shengyi Wang <txyyss@gmail.com>
2017-09-12 15:41:31 +0800

    The proof of makeSet is done.

--- sample_mark/spatial_array_graph.v
+++ sample_mark/spatial_array_graph.v
@@ -15,7 +15,7 @@ Instance SAGA_VST: SpatialArrayGraphAssum mpred. Proof. refine (Build_SpatialArr
 
 Definition vgamma2cdata (rpa : nat * Z) : reptype vertex_type :=
   match rpa with
-  | (r, pa) => (Vint (Int.repr (Z.of_nat r)), Vint (Int.repr pa))
+  | (r, pa) => (Vint (Int.repr pa), Vint (Int.repr (Z.of_nat r)))
   end.
 
 Instance SAG_VST (sh: share): SpatialArrayGraph pointer_val mpred := fun pt lst => data_at sh (tarray vertex_type (Z.of_nat (length lst))) (map vgamma2cdata lst) (pointer_val_val pt).
