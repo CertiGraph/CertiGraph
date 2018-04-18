5d591f9272bbf7e94d79d95f8c7e1b0d61384068
Shengyi Wang <txyyss@gmail.com>
2018-04-18 13:36:02 -0400

    verif_unionfind_iter and verif_unionfind_iter_rank is done.

--- sample_mark/spatial_graph_uf_iter.v
+++ sample_mark/spatial_graph_uf_iter.v
@@ -1,14 +1,8 @@
-Require Import RamifyCoq.lib.EquivDec_ext.
-Require Import RamifyCoq.msl_ext.abs_addr.
-Require Import RamifyCoq.msl_ext.seplog.
+Require Import RamifyCoq.msl_ext.iter_sepcon.
 Require Import RamifyCoq.msl_application.Graph.
 Require Import RamifyCoq.msl_application.GList.
 Require Import VST.veric.SeparationLogic.
-Require Import RamifyCoq.veric_ext.SeparationLogic.
-Require Import RamifyCoq.msl_ext.alg_seplog.
 Require Import RamifyCoq.sample_mark.env_unionfind_iter.
-Require Import RamifyCoq.veric_ext.SeparationLogic.
-Require Import RamifyCoq.floyd_ext.DataatSL.
 Require Import RamifyCoq.floyd_ext.share.
 
 Local Open Scope logic.
@@ -32,7 +26,7 @@ Defined.
 End pSGG_VST.
 
 Instance pSGG_VST: pPointwiseGraph_GList.
-  refine (Build_pPointwiseGraph_GList pointer_val NullPointer mpred SGBA_VST).
+  refine (Build_pPointwiseGraph_GList pointer_val NullPointer SGBA_VST).
 Defined.
 
 Definition vgamma2cdata (rpa : nat * addr) : reptype node_type :=
@@ -45,16 +39,18 @@ Section sSGG_VST.
   Definition binode (sh: share) (p: addr) (rpa: nat * addr): mpred :=
     data_at sh node_type (vgamma2cdata rpa) (pointer_val_val p).
 
-  Instance SGP_VST (sh: share) : PointwiseGraphPred addr (addr * unit) (nat * addr) unit pred.
+  Instance SGP_VST (sh: share) : PointwiseGraphPred addr (addr * unit) (nat * addr) unit mpred.
   refine (Build_PointwiseGraphPred _ _ _ _ _ (binode sh) (fun _ _ => emp)).
   Defined.
 
+  (*
   Instance MSLstandard sh : MapstoSepLog (AAV (SGP_VST sh)) (binode sh).
   Proof.
     intros. apply mkMapstoSepLog. intros.
     apply derives_precise with (memory_block sh (sizeof node_type) (pointer_val_val p)); [| apply memory_block_precise].
     apply exp_left; intros [? ?]. unfold binode. apply data_at_memory_block.
   Defined.
+   *)
 
   Lemma sepcon_unique_vertex_at sh: writable_share sh -> sepcon_unique2 (@vertex_at _ _ _ _ _ (SGP_VST sh)).
   Proof.
@@ -64,11 +60,8 @@ Section sSGG_VST.
     rewrite data_at_isptr.
     normalize.
     apply data_at_conflict.
-    + apply sepalg.nonidentity_nonunit.
-      apply readable_nonidentity, writable_readable.
-      auto.
-    + change (sizeof node_type) with 8.
-      apply pointer_range_overlap_refl; auto; omega.
+    + apply readable_nonidentity, writable_readable. auto.
+    + change (sizeof node_type) with 8. omega.
   Qed.
 
 Instance SGA_VST (sh: share) : PointwiseGraphAssum (SGP_VST sh).
@@ -92,7 +85,7 @@ End sSGG_VST.
 Hint Extern 10 (@sepcon_unique2 _ _ _ _ _ (@vertex_at _ _ _ _ _ _)) => apply sepcon_unique_vertex_at; auto.
 
 Instance sSGG_VST (sh: wshare): @sPointwiseGraph_GList pSGG_VST nat unit.
-  refine (Build_sPointwiseGraph_GList pSGG_VST _ _ (SGP_VST sh) (SGA_VST sh) (SGAvs_VST sh) (SGAvn_VST sh)).
+  refine (Build_sPointwiseGraph_GList pSGG_VST _ _ _ (SGP_VST sh) (SGA_VST sh) (SGAvs_VST sh) (SGAvn_VST sh)).
 Defined.
 
 Global Opaque pSGG_VST sSGG_VST.
