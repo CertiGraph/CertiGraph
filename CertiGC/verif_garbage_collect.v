a5d10fcf8619968902fa57d1e7a172ee7bd6ea12
Shengyi Wang <txyyss@gmail.com>
2018-05-20 19:27:57 -0400

    Initial stub for garbage collector.

---
+++ CertiGC/verif_garbage_collect.v
@@ -0,0 +1,13 @@
+Require Import RamifyCoq.CertiGC.gc_spec.
+
+Lemma body_garbage_collect:
+  semax_body Vprog Gprog f_garbage_collect garbage_collect_spec.
+Proof.
+  start_function.
+  unfold thread_info_rep. if_tac.
+  - forward. forward_if. 2: inversion H0.
+    forward_call (proj1_sig rsh, gv). Intros hp. destruct hp as [h p].
+    simpl fst. simpl snd. forward.
+    admit.
+  - Intros. forward. entailer!.
+Abort.
