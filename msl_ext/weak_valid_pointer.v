b123616a5e1d6d31f615058b2ac17b25e5c60baa
Shengyi Wang <txyyss@gmail.com>
2018-10-03 10:47:23 +0800

    Change some code due to the change in VST.

--- msl_ext/weak_valid_pointer.v
+++ msl_ext/weak_valid_pointer.v
@@ -3,7 +3,7 @@ Require Import VST.msl.normalize.
 Require Import VST.veric.compcert_rmaps.
 Require Import VST.msl.msl_standard.
 Require Import VST.veric.res_predicates.
-Require Import VST.veric.seplog.
+Require Import VST.veric.Clight_seplog.
 Require Import VST.veric.tycontext.
 Require Import VST.veric.expr2.
 Require Import VST.veric.expr_lemmas.
