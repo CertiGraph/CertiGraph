f12113732d05b44ebec53aa33268ef4b0e03be50
Shengyi Wang <txyyss@gmail.com>
2018-10-23 16:34:06 +0800

    More progress in verif_garbage_collect.v.

--- CertiGC/verif_garbage_collect.v
+++ CertiGC/verif_garbage_collect.v
@@ -1,5 +1,23 @@
 Require Import RamifyCoq.CertiGC.gc_spec.
 
+Lemma sem_sub_pp_total_space: forall s,
+    isptr (space_start s) ->
+    force_val
+      (sem_sub_pp int_or_ptr_type
+                  (offset_val (WORD_SIZE * total_space s) (space_start s)) 
+                  (space_start s)) = Vint (Int.repr (total_space s)).
+Proof.
+  intros. destruct (space_start s); try contradiction. simpl. destruct (eq_block b b).
+  2: exfalso; apply n; reflexivity. inv_int i.
+  rewrite ptrofs_add_repr, ptrofs_sub_repr.
+  replace (ofs + WORD_SIZE * total_space s - ofs) with
+      (WORD_SIZE * total_space s)%Z by omega. simpl.
+  pose proof (total_space_signed_range s). unfold Ptrofs.divs.
+  rewrite !Ptrofs.signed_repr by rep_omega. unfold vptrofs, Archi.ptr64.
+  rewrite WORD_SIZE_eq, Z.mul_comm, Z.quot_mul, ptrofs_to_int_repr by omega.
+  reflexivity.  
+Qed.
+
 Lemma body_garbage_collect:
   semax_body Vprog Gprog f_garbage_collect garbage_collect_spec.
 Proof.
@@ -27,7 +45,7 @@ Proof.
      PROP (super_compatible (g', t_info', roots') f_info outlier;
            firstn_gen_clear g' (Z.to_nat i);
            garbage_collect_loop f_info (nat_inc_list (Z.to_nat i)) roots g roots' g';
-           ti_size_spec t_info'; graph_has_gen g (Z.to_nat i))
+           ti_size_spec t_info'; graph_has_gen g' (Z.to_nat i))
      LOCAL (temp _h (ti_heap_p t_info'); temp _fi fi; temp _ti ti;
             gvars gv)
      SEP (thread_info_rep sh t_info' ti;
@@ -35,8 +53,8 @@ Proof.
           fun_info_rep rsh f_info fi;
           outlier_rep outlier;
           graph_rep g')).
-  - Exists g roots t_info. destruct H1 as [? [? [? ?]]].
-    destruct H0 as [? [? [? [? ?]]]]. pose proof (graph_has_gen_O g). entailer!. split.
+  - Exists g roots t_info. destruct H1 as [? [? [? ?]]]. destruct H0 as [?[?[?[? ?]]]].
+    pose proof (graph_has_gen_O g). entailer!. split.
     + red. intros. omega.
     + unfold nat_inc_list. simpl. constructor.
   - cbv beta. Intros g' roots' t_info'. unfold thread_info_rep. Intros.
@@ -68,7 +86,21 @@ Proof.
         sep_apply (valid_pointer_weak
                      (space_start (Znth (i + 1) (spaces (ti_heap t_info'))))).
         apply extend_weak_valid_pointer. Opaque denote_tc_test_eq.
-    + assert (0 <= i + 1 < Zlength (spaces (ti_heap t_info'))) by omega.
-      (* pose proof (space_start_isptr *)
-      (* * rewrite Znth_map. unfold space_tri. entailer!. *)
+    + assert (0 <= i < Zlength (spaces (ti_heap t_info'))) by omega.
+      pose proof (space_start_isptr _ _ _ (proj1 H7) H15 H11). forward.
+      1: entailer!; rewrite Znth_map by assumption; unfold space_tri;
+        apply isptr_is_pointer_or_null, isptr_offset_val'; assumption.
+      rewrite Znth_map by assumption. unfold space_tri at 1. forward.
+      1: entailer!; rewrite Znth_map by assumption; unfold space_tri;
+        apply isptr_is_pointer_or_null; assumption.
+      rewrite Znth_map by assumption. unfold space_tri at 1. forward.
+      1: entailer!; destruct (space_start (Znth i (spaces (ti_heap t_info'))));
+        try contradiction; simpl; unfold denote_tc_samebase;
+          apply prop_right; simpl; destruct (peq b b); simpl; [|apply n]; auto.
+      simpl sem_binary_operation'.
+      change (Tpointer tvoid {| attr_volatile := false; attr_alignas := Some 2%N |})
+        with int_or_ptr_type. remember (Znth i (spaces (ti_heap t_info'))).
+      rewrite sem_sub_pp_total_space by assumption. subst s.
+      pose proof (ti_size_gen _ _ _ (proj1 H7) H11 H10). unfold gen_size in H17.
+      rewrite nth_space_Znth, Z2Nat.id in H17 by omega. rewrite H17. clear H17.
 Abort.
