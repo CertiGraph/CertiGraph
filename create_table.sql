dd3966d1a63e9c92bf0391182d251f91600fa8ad
Shengyi Wang <txyyss@gmail.com>
2018-12-01 01:11:12 +0800

    1. Add Class to stat script. 2. VST is changed again.  We can verify conversion function of int_or_ptr_type now

--- create_table.sql
+++ create_table.sql
@@ -1,4 +1,4 @@
-create table definition (name varchar(100), dir varchar(100), src varchar(100));
-create table theorem (name varchar(100), dir varchar(100), src varchar(100));
+create table def (name varchar(100), dir varchar(100), src varchar(100));
+create table thm (name varchar(100), dir varchar(100), src varchar(100));
 create table def_occur (name varchar(100), dir varchar(100), file varchar(100), times int);
 create table thm_occur (name varchar(100), dir varchar(100), file varchar(100), times int);
