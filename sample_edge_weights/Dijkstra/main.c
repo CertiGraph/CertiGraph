466e28dbea1c313b77d35a371a443b2436d08ba5
Anshuman Mohan <anshumanmohan@live.com>
2019-06-14 16:53:33 +0800

    Added an implementation of Dijkstra's algorithm for single-source-all-destinations. Some thoughts also about the spec etc. The Coq graph may need to be moved.

---
+++ sample_edge_weights/Dijkstra/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  Dijkstra
+//
+//  Created by Anshuman Mohan on 14/6/19.
+//  Copyright © 2019 Anshuman Mohan. All rights reserved.
+//
+
+#include "dijkstra.h"
+
+int main(int argc, const char * argv[])
+{
+    setup();
+    print_graph();
+    dijkstra();
+    return 0;
+}
