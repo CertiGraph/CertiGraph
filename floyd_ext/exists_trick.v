71a2bc77d83d55f80bfb1e7f5b3aff6525181cba
Anshuman Mohan <anshumanmohan@live.com>
2019-07-03 14:18:06 +0800

    Cleaned up by discarding files that are not used at present. No other renaming or editing.

--- floyd_ext/exists_trick.v
+++
@@ -1,8 +0,0 @@
-Lemma exists_aux: forall P: Prop, (exists H: P, let H' := H in True) -> P.
-Proof.
-  intros.
-  destruct H; auto.
-Qed.
-
-Ltac gather_current_goal_with_evar := apply exists_aux; eexists; exact I.
-
