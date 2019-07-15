0530c5c91e65a30b113ba36de6eee9a3fc6c47ba
Anshuman Mohan <anshumanmohan@live.com>
2019-07-15 10:49:14 +0800

    Stashing some progress in Dijkstra

--- msl_application/DijkstraGraph.v
+++ msl_application/DijkstraGraph.v
@@ -11,17 +11,18 @@ Require Import RamifyCoq.graph.FiniteGraph.
 
 Section DijkstraGraph.
 
+  Definition SIZE := 8.
+
   Context {V E: Type}.
   Context {VE: EqDec V eq}.
   Context {EE: EqDec E eq}.
 
-  Definition LV: Type := nat.
-  Definition LE : Type := nat.
-  Definition LG: Type := unit.
-  
-  Local Coercion pg_lg: LabeledGraph >-> PreGraph.
-  Local Coercion lg_gg: GeneralGraph >-> LabeledGraph.
-  
+  Definition LE : Type := option nat.
+  Record LV: Type := { edges: list LE; edges_range: 0 <= length edges < SIZE }.
+  Record LG: Type := { src: LV; size: nat }.
+      
+  Coercion pg_lg: LabeledGraph >-> PreGraph.
+  Coercion lg_gg: GeneralGraph >-> LabeledGraph. 
   Class FinNat (g: LabeledGraph V E LV LE LG) :=
     {
       fin: FiniteGraph g;
@@ -32,10 +33,21 @@ Section DijkstraGraph.
   Definition LGraph := LabeledGraph V E LV LE LG.
   Definition Graph := (GeneralGraph V E LV LE LG (fun g => FinNat g)).
 
-  Definition path_cost (g: Graph) (p : @path V E) : LE :=
+  Fixpoint choose {A : Type} (l : list (option A)) : list A :=
+    match l with
+    | nil => nil
+    | Some x :: tl => x :: choose tl
+    | None :: tl => choose tl
+    end.
+
+  Definition allTrue {A : Type} (l : list (option A)) : bool :=
+    fold_right (fun x acc => match x with Some _ => acc | _ => false end) true l.
+
+  (* assuming that allTrue will be in the vvalid of path *)
+  Definition path_cost (g: Graph) (p : @path V E) : nat :=
     match p with
     | (v, nil) => 0
-    | (v, edges) => fold_left Nat.add (map (elabel g) edges) 0
+    | (v, edges) => fold_left Nat.add (choose (map (elabel g) edges)) 0
     end.
 
 End DijkstraGraph.
