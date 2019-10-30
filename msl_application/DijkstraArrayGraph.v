574bf13079aab2c8b29bc6abea1222d2a8425393
Anshuman Mohan <anshumanmohan@live.com>
2019-10-30 14:58:01 +0800

    some refactoring

--- msl_application/DijkstraArrayGraph.v
+++ msl_application/DijkstraArrayGraph.v
@@ -59,15 +59,35 @@ Fixpoint Z_inc_list (n: nat) : list Z :=
   | S n' => Z_inc_list n' ++ (Z.of_nat n' :: nil)
   end.
 
+Lemma Z_inc_list_length: forall n, Zlength (Z_inc_list n) = Z.of_nat n.
+Proof.
+  intros. induction n. trivial.
+  simpl Z_inc_list. rewrite Zlength_app. rewrite IHn.
+  rewrite Nat2Z.inj_succ. unfold Zlength. simpl. omega.
+Qed.
 
-  Class Fin (g: LabeledGraph VType EType LV LE LG) :=
-    { fin: FiniteGraph g;
-      (* pos: Z_inc_list (Z.to_nat SIZE) *)
-    }.
+Lemma Z_inc_list_eq: forall i len,
+    0 <= i < (Z.of_nat len) -> i = Znth i (Z_inc_list len).
+Proof.
+  intros. generalize dependent i. induction len.
+  1: intros; exfalso; destruct H; rewrite Nat2Z.inj_0 in H0; omega.
+  intros. simpl. rewrite Nat2Z.inj_succ in H. destruct H.
+  apply Zlt_succ_le in H0. apply Zle_lt_or_eq in H0. destruct H0.
+  - rewrite app_Znth1. apply IHlen. omega. now rewrite Z_inc_list_length.
+  - rewrite app_Znth2 by (rewrite Z_inc_list_length; omega).
+    rewrite H0 at 2. rewrite Z_inc_list_length. simpl.
+    replace (Z.of_nat len - Z.of_nat len) with 0 by omega.
+    rewrite Znth_0_cons; trivial.
+Qed.
 
 
-  Definition LGraph := LabeledGraph VType EType LV LE LG.
-  Definition Graph := (GeneralGraph VType EType LV LE LG (fun g => Fin g)).
+Class Fin (g: LabeledGraph VType EType LV LE LG) :=
+  { fin: FiniteGraph g;
+    (* pos: Z_inc_list (Z.to_nat SIZE) *)
+  }.
+
+Definition LGraph := LabeledGraph VType EType LV LE LG.
+Definition Graph := (GeneralGraph VType EType LV LE LG (fun g => Fin g)).
 
 
 (* Definition Graph := (@Graph VType EType _ _ LV LE LG). *)
@@ -77,7 +97,7 @@ Fixpoint Z_inc_list (n: nat) : list Z :=
 (* Moving on to Spatial Rep *)
 
 Section SpaceDijkstraArrayGraph.
-
+  
   Class SpatialDijkstraArrayGraphAssum (Pred : Type):=
   {
     SGP_ND: NatDed Pred;
@@ -101,36 +121,37 @@ Section SpaceDijkstraArrayGraph.
     | None :: tl => choose tl
     end.
   
-(* TODO: move this to vvalid of a Dijk path *)
-Definition allTrue {A : Type} (l : list (option A)) : bool :=
-  fold_right (fun x acc => match x with Some _ => acc | _ => false end) true l.
+  (* TODO: move this to vvalid of a Dijk path *)
+  Definition allTrue {A : Type} (l : list (option A)) : bool :=
+    fold_right (fun x acc => match x with Some _ => acc | _ => false end) true l.
+  
+  (* assuming that allTrue will be in the vvalid of path *)
+  Definition path_cost (g: Graph) (p : @path VType EType) : Z :=
+    match p with
+    | (v, nil) => 0
+    | (v, edges) => fold_left Z.add (choose (map (elabel g) edges)) 0
+    end.
+  
 
-(* assuming that allTrue will be in the vvalid of path *)
-Definition path_cost (g: Graph) (p : @path VType EType) : Z :=
-  match p with
-  | (v, nil) => 0
-  | (v, edges) => fold_left Z.add (choose (map (elabel g) edges)) 0
-  end.
-
-
-Definition vert_rep (g : Graph) (v : LV) : list Z :=
-  map (fun x => match x with Some x => x | None => inf end) v.
-
-(* from Graph to list (list Z) *)
-Definition graph_to_mat (g : Graph) : list (list Z) :=
-  map (vert_rep g) (map (vlabel g) (Z_inc_list (Z.to_nat SIZE))).
-
-Definition list_rep listaddr contents_mat index :=
-  abstract_data_at listaddr (Znth index contents_mat).
-
-(* spatial representation of the DijkstraGraph *)
-Definition graph_rep (g : Graph) (a : Addr)  :=
-  abstract_data_at a (concat (graph_to_mat g)).
-
+  Definition vert_rep (g : Graph) (v : LV) : list Z :=
+    map (fun x => match x with Some x => x | None => inf end) v.
+  
+  (* from Graph to list (list Z) *)
+  Definition graph_to_mat (g : Graph) : list (list Z) :=
+    map (vert_rep g) (map (vlabel g) (Z_inc_list (Z.to_nat SIZE))).
+  
+  Definition list_rep listaddr contents_mat index :=
+    abstract_data_at listaddr (Znth index contents_mat).
+  
+  (* spatial representation of the DijkstraGraph *)
+  Definition graph_rep (g : Graph) (a : Addr)  :=
+    abstract_data_at a (concat (graph_to_mat g)).
+  
 (* But the above is a little annoying.
    I would prefer to make an iter_sepcon of the various list_reps.
    The issue right now is that I can't get the lists to calculate
    their own addresses. 
 *)
-
+  
 End SpaceDijkstraArrayGraph.
+
