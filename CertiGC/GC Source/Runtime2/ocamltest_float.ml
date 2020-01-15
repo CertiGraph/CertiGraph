fdbac9ecace83310e75bf5d2ac41c130b7c903be
Anshuman Mohan <anshumanmohan@live.com>
2020-01-15 13:59:19 +0800

    Pushing WX's Runtime2, and some progress in dijkstra. Just as a quick sanity check, I am now going to go back and weaken "prev"'s info, and push it all the way through.

---
+++ CertiGC/GC Source/Runtime2/ocamltest_float.ml
@@ -0,0 +1,23 @@
+type tree = Leaf | Node of float * tree * tree
+
+let rec insert (key: float) (t: tree) : tree =
+ match t with
+  Leaf -> Node(key,Leaf,Leaf)
+ | Node(k,left,right) ->
+    if (key<k) then Node(k, insert key left, right)
+    else if (k<key) then Node (k, left, insert key right)
+    else t
+
+let rec build (n: int) (t: tree) : tree =
+ if n>0 then build (n-1) (insert (Random.float 1000.0) t)
+ else t
+
+let rec measure (t: tree) : int =
+ match t with Leaf -> 0 
+            | Node (_,left,right) -> 1 + measure left + measure right
+
+let () =
+  let n = int_of_string (Sys.argv.(1)) in
+  let t = build n Leaf in
+  let s = measure t in
+  (print_string "Tree has "; print_int s; print_string " nodes\n")
