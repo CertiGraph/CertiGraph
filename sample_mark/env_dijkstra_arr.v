b4cb606a89e2e7ce04a3101a4e220ea12488d50d
Anshuman Mohan <anshumanmohan@live.com>
2020-01-28 12:52:47 +0800

    Removed internal comments and todos from auxilliary files. Piped through priq[src] <> inf, and added helpful comments to the proof script.

--- sample_mark/env_dijkstra_arr.v
+++ sample_mark/env_dijkstra_arr.v
@@ -1,6 +1,9 @@
 Require Export VST.floyd.proofauto.
 Require Export RamifyCoq.floyd_ext.closed_lemmas.
 Require Export RamifyCoq.sample_mark.dijkstra.
+(* The above line imports dijkstra.v, 
+   the AST of the C program dijkstra.c 
+ *)
 
 Instance CompSpecs : compspecs. Proof. make_compspecs prog. Defined.
 Definition Vprog : varspecs. mk_varspecs prog. Defined.
