fb7ad56f9767462a28a86fe380a91a0f868c0b28
Anshuman Mohan <anshumanmohan@live.com>
2020-04-28 22:26:02 +0800

    Had forgotten to push verif_priorityqueue.

---
+++ sample_mark/verif_priorityqueue.v
@@ -0,0 +1,207 @@
+Require Import VST.floyd.proofauto.
+Require Import RamifyCoq.sample_mark.dijk_pq_arr_macros.
+Require Import RamifyCoq.sample_mark.priorityqueue.
+Require Import RamifyCoq.sample_mark.priq_utils.
+
+Instance CompSpecs : compspecs. Proof. make_compspecs prog. Defined.
+Definition Vprog : varspecs. mk_varspecs prog. Defined.
+Global Existing Instance CompSpecs.
+
+(** MACROS **)
+
+Lemma inf_eq2: Int.sub (Int.repr 2147483647)
+                       (Int.divs (Int.repr 2147483647)
+                                 (Int.repr SIZE)) = Int.repr inf.
+Proof. compute. trivial. Qed.
+
+(** SPECS **)
+
+(*
+Definition push_spec :=
+  DECLARE _push
+  WITH pq: val, vertex : Z, weight : Z, priq_contents: list Z
+  PRE [tint, tint, tptr tint]
+  PROP (0 <= vertex < SIZE)
+  PARAMS (Vint (Int.repr vertex);
+          Vint (Int.repr weight);
+          pq)
+  GLOBALS ()
+  SEP (data_at Tsh (tarray tint SIZE) (map Vint (map Int.repr priq_contents)) pq)
+  POST [tvoid]
+  PROP ()
+  LOCAL ()
+  SEP (data_at Tsh (tarray tint SIZE)
+               (upd_Znth vertex
+                  (map Vint (map Int.repr priq_contents)) (Vint (Int.repr weight))) pq).
+ *)
+
+Definition pq_emp_spec :=
+  DECLARE _pq_emp
+  WITH pq: val, priq_contents: list Z
+  PRE [tptr tint]
+   PROP (inrange_priq priq_contents)
+   PARAMS (pq)
+   GLOBALS ()
+   SEP (data_at Tsh (tarray tint SIZE) (map Vint (map Int.repr priq_contents)) pq)
+  POST [ tint ]
+   PROP ()
+   LOCAL (temp ret_temp (isEmpty priq_contents))
+   SEP (data_at Tsh (tarray tint SIZE) (map Vint (map Int.repr priq_contents)) pq).
+
+(*
+Definition adjustWeight_spec :=
+  DECLARE _adjustWeight
+  WITH pq: val, vertex : Z, newWeight : Z, priq_contents: list Z
+  PRE [tint, tint, tptr tint]
+  PROP (0 <= vertex < SIZE)
+  PARAMS (Vint (Int.repr vertex);
+          Vint (Int.repr newWeight);
+          pq)
+  GLOBALS ()
+  SEP (data_at Tsh (tarray tint SIZE) (map Vint (map Int.repr priq_contents)) pq)
+  POST [tvoid]
+  PROP ()
+  LOCAL ()
+  SEP (data_at Tsh (tarray tint SIZE)
+               (upd_Znth vertex
+                  (map Vint (map Int.repr priq_contents)) (Vint (Int.repr newWeight))) pq).
+ *)
+
+Definition popMin_spec :=
+  DECLARE _popMin
+  WITH pq: val, priq_contents: list Z
+  PRE [tptr tint]
+   PROP (inrange_priq priq_contents;
+        isEmpty priq_contents = Vzero)
+   PARAMS (pq)
+   GLOBALS ()
+   SEP   (data_at Tsh (tarray tint SIZE) (map Vint (map Int.repr priq_contents)) pq)
+  POST [ tint ]
+   EX rt : Z,
+   PROP (rt = find priq_contents (fold_right Z.min (hd 0 priq_contents) priq_contents) 0)
+   LOCAL (temp ret_temp  (Vint (Int.repr rt)))
+   SEP   (data_at Tsh (tarray tint SIZE) (upd_Znth
+                                            (find priq_contents (fold_right Z.min (Znth 0 priq_contents) priq_contents) 0)
+                                            (map Vint (map Int.repr priq_contents)) (Vint (Int.repr (inf+1)))) pq).
+
+(* Definition Gprog : funspecs := ltac:(with_library prog [push_spec; pq_emp_spec; adjustWeight_spec; popMin_spec]). *)
+
+Definition Gprog : funspecs := ltac:(with_library prog [pq_emp_spec; popMin_spec]).
+
+(** PROOFS **)
+
+(* Lemma body_push: semax_body Vprog Gprog f_push push_spec. *)
+(* Proof. start_function. forward. entailer!. Qed. *)
+
+Lemma body_pq_emp: semax_body Vprog Gprog f_pq_emp pq_emp_spec.
+Proof.
+  start_function.
+  forward_for_simple_bound
+    SIZE
+    (EX i : Z,
+     PROP (isEmpty_Prop (sublist 0 i priq_contents))
+     LOCAL (temp _pq pq)
+     SEP (data_at Tsh (tarray tint SIZE) (map Vint (map Int.repr priq_contents)) pq)).
+  - unfold SIZE; rep_omega.
+  - unfold SIZE; rep_omega.
+  - entailer!.
+  - simpl.
+    assert_PROP (Zlength priq_contents = SIZE). {
+      entailer!. repeat rewrite Zlength_map in H3; auto.
+    }
+    forward; forward_if; forward; entailer!.
+    + rewrite (isEmpty_in priq_contents (Znth i priq_contents)).
+      trivial.
+      apply Znth_In; omega.
+      rewrite <- H1 in H0.
+      pose proof (Forall_Znth _ _ i H0 H).
+      rewrite Int.signed_repr in H3.
+      apply (Z.lt_stepr _ _ _ H3). compute; trivial.
+      simpl in H7. rep_omega.
+    + rewrite (sublist_split 0 i (i+1)); try omega.
+      unfold isEmpty_Prop.
+      rewrite fold_right_app.
+      rewrite sublist_one; try omega. simpl.
+      destruct (Z_lt_dec (Znth i priq_contents) inf).
+      2: unfold isEmpty_Prop in H2; trivial.
+      exfalso.
+      replace 8 with SIZE in H3 by (unfold SIZE; trivial).
+      rewrite inf_eq2 in H3.
+      do 2 rewrite Int.signed_repr in H3.
+      rep_omega.
+      1: compute; split; inversion 1.
+      1,2: rewrite <- H1 in H0; apply (Forall_Znth _ _ i H0) in H; simpl in H; rep_omega.
+  - forward. entailer!.
+    rewrite sublist_same in H0; trivial.
+    2: { symmetry; repeat rewrite Zlength_map in H2.
+         unfold SIZE. simpl in H2. omega. }
+    replace (Vint (Int.repr 1)) with Vone by now unfold Vone, Int.one.
+    symmetry. apply isEmpty_prop_val; trivial.
+Qed.
+
+(* Lemma body_adjustWeight: semax_body Vprog Gprog f_adjustWeight adjustWeight_spec. *)
+(* Proof. start_function. forward. entailer!. Qed. *)
+
+Lemma body_popMin: semax_body Vprog Gprog f_popMin popMin_spec.
+Proof.
+  start_function.
+  assert_PROP (Zlength priq_contents = SIZE). {
+    entailer!. repeat rewrite Zlength_map in H2; auto.
+  }
+  assert (0 <= 0 < Zlength (map Int.repr priq_contents)) by
+      (rewrite Zlength_map; rewrite H1; unfold SIZE; omega).
+  assert (0 <= 0 < Zlength priq_contents) by
+      (rewrite H1; unfold SIZE; omega).
+  forward. forward.
+  forward_for_simple_bound
+    SIZE
+    (EX i : Z,
+     PROP ()
+     LOCAL (temp _minWeight (Vint (Int.repr (fold_right Z.min (Znth 0 priq_contents) (sublist 0 i priq_contents))));
+                        temp _minVertex (Vint (Int.repr (find priq_contents (fold_right Z.min (Znth 0 priq_contents) (sublist 0 i priq_contents)) 0)));
+                        temp _pq pq)
+                 SEP (data_at Tsh (tarray tint SIZE) (map Vint (map Int.repr priq_contents)) pq)).
+  - unfold SIZE; rep_omega.
+  - entailer!. simpl. rewrite find_index.
+    trivial. omega. simpl. unfold not. omega.
+  - forward.
+    assert (0 <= i < Zlength priq_contents) by omega.
+    assert (Int.min_signed <=
+            fold_right Z.min (Znth 0 priq_contents) (sublist 0 i priq_contents) <= Int.max_signed).
+    { apply Forall_fold_min. apply Forall_Znth. omega.
+      rewrite Forall_forall. intros. rewrite In_Znth_iff in H6.
+      destruct H6 as [? [? ?]]. rewrite <- H7.
+      pose proof (Forall_Znth _ _ x0 H6 H).
+      simpl in H8. rep_omega.
+      rewrite Forall_forall. intros. rewrite In_Znth_iff in H6.
+      destruct H6 as [? [? ?]]. rewrite <- H7.
+      apply (Forall_sublist _ 0 i _) in H.
+      apply (Forall_Znth _ _ _ H6) in H.
+      simpl in H. rep_omega.
+    }
+    assert (Int.min_signed <= Znth i priq_contents <= Int.max_signed). {
+      apply (Forall_Znth _ _ _ H5) in H; simpl in H; rep_omega. }
+    forward_if.
+    + forward. forward. entailer!.
+      rewrite (sublist_split 0 i (i+1)) by omega.
+      rewrite (sublist_one i (i+1) priq_contents) by omega.
+      rewrite fold_min_another.
+      rewrite Z.min_r; [|omega].
+      split; trivial. f_equal.
+      rewrite find_index; trivial.
+      apply min_not_in_prev; trivial.
+    + forward. entailer!.
+      rewrite (sublist_split 0 i (i+1)) by omega.
+      rewrite (sublist_one i (i+1) priq_contents) by omega.
+      rewrite fold_min_another.
+      rewrite Z.min_l; [|omega]. split; trivial.
+  - forward.
+    + entailer!. rewrite <- H1.
+      apply find_range.
+      rewrite sublist_same; [|omega..].
+      apply min_in_list; [apply incl_refl | apply Znth_In; omega].
+    + forward.
+      Exists (find priq_contents (fold_right Z.min (hd 0 priq_contents) (sublist 0 SIZE priq_contents)) 0).
+      rewrite sublist_same by omega. entailer!.
+      destruct priq_contents; simpl; auto.
+Qed.
