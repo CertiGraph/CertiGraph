408dc78310d0fc1b056d603d0f2a2cb8f05b6a50
Anshuman Mohan <anshumanmohan@live.com>
2020-04-28 16:47:51 +0800

    Separated out the proof of PQ

--- sample_mark/env_dijkstra_arr.v
+++ sample_mark/env_dijkstra_arr.v
@@ -1,13 +1,10 @@
 Require Export VST.floyd.proofauto.
 Require Export RamifyCoq.floyd_ext.closed_lemmas.
+(* Require Export RamifyCoq.sample_mark.priorityqueue. *)
 Require Export RamifyCoq.sample_mark.dijkstra.
-(* The above line imports dijkstra.v, 
-   the AST of the C program dijkstra.c 
- *)
 
 Instance CompSpecs : compspecs. Proof. make_compspecs prog. Defined.
 Definition Vprog : varspecs. mk_varspecs prog. Defined.
 
 Global Existing Instance CompSpecs.
-
 Definition vertex_type := tint.
