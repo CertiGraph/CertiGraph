05ca321cd628bd246f649bed9f8b0f6ca7a84783
Leow Wei Xiang <leowweixiang1991@gmail.com>
2020-05-12 16:04:32 +0800

    Add missing kruskal files

--- kruskal/env_kruskal_edgelist.v
+++ kruskal/env_kruskal_edgelist.v
@@ -1,12 +1,10 @@
 Require Export VST.floyd.proofauto.
 Require Export RamifyCoq.floyd_ext.closed_lemmas.
-(*Require Export RamifyCoq.sample_mark.unionfind_arr.*)
-(*There are some side effects with loading this version of unionfind_arr which breaks some parts of the proof*)
-Load "./unionfind_arr.v".
+Require Export RamifyCoq.kruskal.kruskal_edgelist.
 
 Instance CompSpecs : compspecs. Proof. make_compspecs prog. Defined.
 Definition Vprog : varspecs. mk_varspecs prog. Defined.
-
 Global Existing Instance CompSpecs.
 
-Definition vertex_type := Tstruct _subset noattr.
+Definition t_struct_edge := Tstruct _edge noattr.
+Definition t_edgearray_graph := Tstruct _graph noattr.
