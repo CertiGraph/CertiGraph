ace3fa4028b7262f59cfc307c1b86a94ce085252
Anshuman Mohan <anshumanmohan@live.com>
2020-05-13 15:01:49 +0800

    More twiddles in Aquinas' code to make it go through start_function

--- sample_mark/binary_heap.c
+++ sample_mark/binary_heap.c
@@ -23,12 +23,14 @@ typedef struct structPQ {
 
 /* I'm assuming a decent compiler will inline the next two functions; if not they can be made #define macros. */
 void exch(unsigned int j, unsigned int k, Item arr[]) {
-  Item t = arr[j];
-  arr[j] = arr[k];
-  arr[k] = t;
+  int priority = arr[j].priority;
+  void* data = arr[j].data;
+  arr[j].priority = arr[k].priority;
+  arr[j].data = arr[k].data;
+  arr[k].priority = priority;
+  arr[k].data = data;
 }
 
-/* If this doesn't work with clightgen, we can use the #define method. */
 int less(unsigned int j, unsigned int k, Item arr[]) {
   return (arr[j].priority <= arr[k].priority);
 }
@@ -40,44 +42,58 @@ void swim(unsigned int k, Item arr[]) {
   }
 }
 
-void sink (unsigned int k, Item arr[], unsigned int last_used) {
-  while (LEFT_CHILD(k) <= last_used) { /* Requirement that capacity <= MAX_SIZE be of reasonable size */
+void sink (unsigned int k, Item arr[], unsigned int first_available) {
+  while (LEFT_CHILD(k) < first_available) { /* Requirement that capacity <= MAX_SIZE be of reasonable size */
     unsigned j = LEFT_CHILD(k);
-    if (j < last_used && less(j + 1, j, arr)) j++; /* careful with j+1 overflow? */
+    if (j+1 < first_available && less(j+1, j, arr)) j++; /* careful with j+1 overflow? */
     if (less(j, k, arr)) break;
     exch(k, j, arr);
     k = j;
   }
 }
 
-void insert(PQ pq, Item x) {
-  if (pq.first_available == pq.capacity) return; /* Hrm, maybe should signal error or grow heap or whatever... */
-  pq.heap_cells[pq.first_available] = x;
-  swim(pq.first_available, pq.heap_cells);
-  pq.first_available++;
+void insert_nc(PQ *pq, Item *x) {
+  pq->heap_cells[pq->first_available].priority = x->priority;
+  pq->heap_cells[pq->first_available].data = x->data;
+  swim(pq->first_available, pq->heap_cells);
+  pq->first_available++;
 }
 
-Item remove_min(PQ pq) {
-  /* if (pq.first_available == ROOT_IDX) return 0; */ /* Not sure what to return here... maybe make return type Item*?  */
-  pq.first_available--;
-  if (pq.first_available == ROOT_IDX) return pq.heap_cells[ROOT_IDX];
-  exch(ROOT_IDX, pq.first_available, pq.heap_cells);
-  Item item = pq.heap_cells[pq.first_available];
-  sink(ROOT_IDX, pq.heap_cells, pq.first_available - 1);
+void insert(PQ *pq, Item *x) {
+  if (pq->first_available == pq->capacity) return; /* Hrm, maybe should signal error or grow heap or whatever... */
+  insert_nc(pq, x);
+}
+
+void remove_min_nc(PQ *pq, Item* item) {
+  pq->first_available--;
+  exch(ROOT_IDX, pq->first_available, pq->heap_cells);
+  item->priority = pq->heap_cells[pq->first_available].priority;
+  item->data = pq->heap_cells[pq->first_available].data;
+  sink(ROOT_IDX, pq->heap_cells, pq->first_available);
+}  
+
+Item* remove_min(PQ *pq) {
+  if (pq->first_available == ROOT_IDX) return 0;
+  Item* item = (Item*) mallocN(sizeof(Item));
+  remove_min_nc(pq, item);
   return item;
 }
 
-unsigned int size(PQ pq) {
-  return pq.first_available;
+unsigned int size(PQ *pq) {
+  return pq->first_available;
 }
 
-unsigned int capacity(PQ pq) {
-  return pq.capacity;
+unsigned int capacity(PQ *pq) {
+  return pq->capacity;
 }
 
-PQ make() { /* could take a size parameter I suppose... */
+PQ* make() { /* could take a size parameter I suppose... */
   Item* arr = (Item*) mallocN(sizeof(Item) * INITIAL_SIZE);
-  return (PQ) {.capacity = INITIAL_SIZE, .first_available = 0, .heap_cells = arr};
+  PQ *pq = (PQ*) mallocN(sizeof(PQ));
+  pq->capacity = INITIAL_SIZE;
+  pq->first_available = 0;
+  pq->heap_cells = arr;
+  return pq;
 }
 
 /* could imagine adding some additonal functions:
