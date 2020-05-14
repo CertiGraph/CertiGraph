f25d400e4385a66aac1a289012ca898c4b708218
Aquinas <hobor@comp.nus.edu.sg>
2020-05-14 18:14:47 +0800

    Work on verification.

--- sample_mark/verif_binary_heap.v
+++ sample_mark/verif_binary_heap.v
@@ -1,7 +1,66 @@
 Require Import VST.floyd.proofauto.
 Require Import RamifyCoq.sample_mark.binary_heap_model.
 Require Import RamifyCoq.sample_mark.binary_heap.
+Require Import RelationClasses.
 (* Require Import VST.floyd.sublist. *)
+
+(* Relation on heap items. *)
+Definition heap_item : Type := (int * int)%type.
+Definition cmp (a b : heap_item) : bool :=
+  negb (Int.lt (fst b) (fst a)).
+Definition cmp_rel (a b : heap_item) : Prop :=
+  cmp a b = true.
+Lemma cmp_dec: forall a a', {cmp_rel a a'} + {~cmp_rel a a'}.
+Proof.
+  intros [? ?] [? ?]. unfold cmp_rel, cmp. simpl. case (Int.lt i1 i); simpl; auto.
+Qed. 
+Instance cmp_po: PreOrder cmp_rel.
+Proof.
+  constructor. intros [? ?]. red. unfold cmp. simpl. case_eq (Int.lt i i); auto; intro. exfalso.
+  apply lt_inv in H. lia.
+  intros [? ?] [? ?] [? ?]. unfold cmp_rel, cmp. simpl. 
+  case_eq (Int.lt i3 i); auto.
+  case_eq (Int.lt i1 i); auto.
+  case_eq (Int.lt i3 i1); auto. simpl.
+  intros ? ? ?. exfalso.
+  apply lt_inv in H1.
+  apply lt_false_inv in H.
+  apply lt_false_inv in H0.
+  lia.
+Qed.
+Lemma cmp_linear: forall a b,
+  cmp_rel a b \/ cmp_rel b a.
+Proof.
+  intros [? ?] [? ?]. unfold cmp_rel, cmp; simpl.
+  case_eq (Int.lt i1 i); auto. intro. 
+  right.
+  case_eq (Int.lt i i1); auto. intro. exfalso. 
+  apply lt_inv in H. apply lt_inv in H0.
+  lia.
+Qed.
+
+(* Not sure if it's a great idea to expose the capacity inside the abstraction boundary. *)
+Definition heap : Type := (nat * list heap_item)%type.
+Definition heap_capacity (h : heap) : Z := Z.of_nat (fst h).
+Definition heap_items (h : heap) : list heap_item := snd h.
+Definition heap_size (h : heap) : Z := Zlength (heap_items h).
+
+(*
+Definition heap_permutation (h1 h2 : heap) : Prop :=
+  heap_capacity h1 = heap_capacity h2 /\ Permutation (heap_items h1) (heap_items h2).
+*)
+
+Definition heap_ordered := binary_heap_model.heapOrdered heap_item cmp_rel.
+Definition weak_heap_ordered_bottom_up (L : list heap_item) (x : Z) := 
+  binary_heap_model.weak_heapOrdered2 heap_item cmp_rel L (Z.to_nat x).
+Definition weak_heap_ordered_top_down (L : list heap_item) (x : Z) := 
+  binary_heap_model.weak_heapOrdered heap_item cmp_rel L (Z.to_nat x).
+Definition swim1 := binary_heap_model.swim1 heap_item cmp_rel cmp_dec.
+Definition swim := binary_heap_model.swim heap_item cmp_rel cmp_dec.
+Definition insert := binary_heap_model.insert heap_item cmp_rel cmp_dec.
+Definition sink1 := binary_heap_model.sink1 heap_item cmp_rel cmp_dec.
+Definition sink := binary_heap_model.sink heap_item cmp_rel cmp_dec.
+Definition remove_min := binary_heap_model.remove_min heap_item cmp_rel cmp_dec.
 
 (* This may get bundled elsewhere at some point. *)
 Instance CompSpecs : compspecs. Proof. make_compspecs prog. Defined.
@@ -9,25 +68,265 @@ Definition Vprog : varspecs. mk_varspecs prog. Defined.
 Global Existing Instance CompSpecs.
 
 Definition t_item := Tstruct _structItem noattr.
+Definition t_pq := Tstruct _structPQ noattr.
 
-Definition harray (contents : list (Z * Z)) (ptr : val) : mpred :=
-  data_at Tsh (tarray t_item (Zlength contents)) (map (fun p => (Vint (Int.repr (fst p)), Vint (Int.repr (snd p)))) contents) ptr.
+Definition heap_item_rep (i : heap_item) : reptype t_item :=
+  (Vint (fst i), Vint (snd i)).
+
+Definition hitem (i : heap_item) : val -> mpred :=
+  data_at Tsh t_item (heap_item_rep i).
+
+Definition harray (contents : list heap_item) : val -> mpred :=
+  data_at Tsh (tarray t_item (Zlength contents)) (map heap_item_rep contents).
+
+Definition valid_pq (pq : val) (h: heap): mpred :=
+  EX arr : val, EX junk: list heap_item,
+    (!! heap_ordered (heap_items h)) && (!! (Zlength ((heap_items h) ++ junk) = heap_capacity h)) &&
+    (data_at Tsh t_pq (Vint (Int.repr (heap_capacity h)), (Vint (Int.repr (heap_size h)), arr)) pq *
+       harray ((heap_items h) ++ junk) arr).
 
 Definition exch_spec :=
-  DECLARE _exch
-  WITH i : Z, j : Z, arr: val, arr_contents: list (Z * Z)
+  DECLARE _exch WITH i : Z, j : Z, arr: val, arr_contents: list heap_item
   PRE [tuint, tuint, tptr t_item]
-  PROP (0 <= i < Zlength arr_contents; 0 <= j < Zlength arr_contents)
-  PARAMS (Vint (Int.repr i); Vint (Int.repr j); arr)
-  GLOBALS ()
-  SEP (harray arr_contents arr)
+    PROP (0 <= i < Zlength arr_contents; 0 <= j < Zlength arr_contents)
+    PARAMS (Vint (Int.repr i); Vint (Int.repr j); arr)
+    GLOBALS ()
+    SEP (harray arr_contents arr)
   POST [tvoid]
-  PROP ()
-  LOCAL ()
-  SEP (harray (exchange arr_contents (Z.to_nat i) (Z.to_nat j)) arr).
+    PROP ()
+    LOCAL ()
+    SEP (harray (exchange arr_contents (Z.to_nat i) (Z.to_nat j)) arr).
+
+Definition less_spec :=
+  DECLARE _less WITH i : Z, j : Z, arr: val, arr_contents: list heap_item
+  PRE [tuint, tuint, tptr t_item]
+    PROP (0 <= i < Zlength arr_contents; 0 <= j < Zlength arr_contents)
+    PARAMS (Vint (Int.repr i); Vint (Int.repr j); arr)
+    GLOBALS ()
+    SEP (harray arr_contents arr)
+  POST [tint]
+    PROP ()
+    LOCAL (temp ret_temp (Val.of_bool (cmp (Znth i arr_contents) (Znth j arr_contents))))
+    SEP (harray arr_contents arr).
+
+Definition swim_spec :=
+  DECLARE _swim WITH i : Z, arr: val, arr_contents: list heap_item
+  PRE [tuint, tptr t_item]
+    PROP (0 <= i < Zlength arr_contents; weak_heap_ordered_bottom_up arr_contents i)
+    PARAMS (Vint (Int.repr i); arr)
+    GLOBALS ()
+    SEP (harray arr_contents arr)
+  POST [tvoid]
+    EX arr_contents' : list (int * int),
+      PROP (heap_ordered arr_contents' /\ Permutation arr_contents arr_contents')
+      LOCAL ()
+      SEP (harray arr_contents' arr).
+
+Definition sink_spec :=
+  DECLARE _sink WITH i : Z, arr: val, arr_contents: list heap_item
+  PRE [tuint, tptr t_item]
+    PROP (0 <= i < Zlength arr_contents; weak_heap_ordered_top_down arr_contents i)
+    PARAMS (Vint (Int.repr i); arr)
+    GLOBALS ()
+    SEP (harray arr_contents arr)
+  POST [tvoid]
+    EX arr_contents' : list heap_item,
+      PROP (heap_ordered arr_contents' /\ Permutation arr_contents arr_contents')
+      LOCAL ()
+      SEP (harray arr_contents' arr).
+
+Definition size_spec := 
+  DECLARE _size WITH pq : val, h : heap
+  PRE [tptr t_pq]
+    PROP ()
+    PARAMS (pq)
+    GLOBALS ()
+    SEP (valid_pq pq h)
+  POST [tuint]
+    PROP ()
+    LOCAL (temp ret_temp (Vint (Int.repr (heap_size h))))
+    SEP (valid_pq pq h).
+
+Definition capacity_spec := 
+  DECLARE _capacity WITH pq : val, h : heap
+  PRE [tptr t_pq]
+    PROP ()
+    PARAMS (pq)
+    GLOBALS ()
+    SEP (valid_pq pq h)
+  POST [tuint]
+    PROP ()
+    LOCAL (temp ret_temp (Vint (Int.repr (heap_capacity h))))
+    SEP (valid_pq pq h).
+
+Definition remove_min_nc_spec :=
+  DECLARE _remove_min_nc WITH pq : val, h : heap, i : val, iv : heap_item
+  PRE [tptr t_pq, tptr t_item]
+    PROP (heap_size h > 0)
+    PARAMS (pq; i)
+    GLOBALS ()
+    SEP (valid_pq pq h; hitem iv i)
+  POST [tvoid]
+  EX h', EX iv' : heap_item,
+    PROP (heap_capacity h = heap_capacity h';
+          Permutation (heap_items h) (iv' :: heap_items h');
+          Forall (cmp_rel iv') (heap_items h'))
+    LOCAL ()
+    SEP (valid_pq pq h'; hitem iv' i).
+
+Definition insert_nc_spec :=
+  DECLARE _insert_nc WITH pq : val, h : heap, i : val, iv : heap_item
+  PRE [tptr t_pq, tptr t_item]
+    PROP (heap_size h < heap_capacity h)
+    PARAMS (pq; i)
+    GLOBALS ()
+    SEP (valid_pq pq h; hitem iv i)
+  POST [tvoid]
+  EX h',
+    PROP (heap_capacity h = heap_capacity h';
+          Permutation (iv :: heap_items h) (heap_items h'))
+    LOCAL ()
+    SEP (valid_pq pq h'; hitem iv i).
 
 Definition Gprog : funspecs :=
-         ltac:(with_library prog [ exch_spec ]).
+         ltac:(with_library prog [ exch_spec ; less_spec ; swim_spec ; sink_spec ; 
+                                   remove_min_nc_spec ;
+                                   size_spec ; capacity_spec ]).
+
+Lemma harray_split: forall L1 L2 ptr,
+  harray (L1 ++ L2) ptr = 
+  ((harray L1 ptr) * 
+   (harray L2 (field_address0 (tarray t_item (Zlength (L1 ++ L2))) [ArraySubsc (Zlength L1)] ptr)))%logic.
+Proof.
+  intros. unfold harray.
+  rewrite map_app.
+  erewrite split2_data_at_Tarray_app.
+  2: rewrite Zlength_map; reflexivity. 2: rewrite Zlength_app, Zlength_map; lia.
+  rewrite Zlength_app.
+  replace (Zlength L1 + Zlength L2 - Zlength L1) with (Zlength L2) by lia.
+  trivial.
+Qed.
+
+Set Nested Proofs Allowed.
+Lemma Zlength_exchange : forall A (L : list A) i j,
+  Zlength (exchange L i j) = Zlength L.
+Proof.
+  intros. do 2 rewrite Zlength_correct. rewrite exchange_length. trivial.
+Qed.
+
+Lemma body_remove_min_nc: semax_body Vprog Gprog f_remove_min_nc remove_min_nc_spec.
+Proof.
+  start_function.
+  unfold valid_pq.
+  Intros arr junk.
+  rewrite harray_split.
+  Intros.
+(* Why doesn't "frame_SEP 0 1 3" work here? *)
+Admitted.
+
+(*
+Search 
+Check semax_seq'.
+eapply semax_seq'.
+eapply semax_seq.
+  Intros arr junk.
+  assert (Hj: 0 <= Zlength junk) by apply Zlength_nonneg. unfold heap_size in *. rewrite Zlength_app in *.
+  forward.
+  forward.
+  forward.
+  forward.
+  unfold harray. entailer!.
+  forward_call (0, heap_size h - 1, arr, (heap_items h) ++ junk). {
+    autorewrite with sublist in *.
+    unfold heap_size in *.
+    lia. }
+  forward.
+  forward.
+  unfold harray.
+  forward.
+  entailer!. rewrite Zlength_exchange. rewrite Zlength_app. unfold heap_size in *. lia.
+  entailer!. admit.
+  unfold hitem.
+  forward.
+  forward.
+  forward.
+  forward.
+  entailer!. rewrite Zlength_exchange. rewrite Zlength_app. unfold heap_size in *. lia.
+  entailer!. admit.
+  forward.
+  forward.
+  forward.
+hint.
+deadvars!.
+
+fold harray.
+  forward.
+  Search Zlength length.
+  apply 
+ rewrite 
+
+
+split.
+lia.
+
+ unfold heap_size in *; lia.
+hint.
+
+
+  rewrite harray_split.
+hint.
+Intros.
+
+frame_SEP 1.
+forward.
+  
+  admit.
+  unfold heap_size in *. lia.
+
+  u
+
+Admitted.
+*)
+
+Lemma body_less: semax_body Vprog Gprog f_less less_spec.
+Proof.
+  start_function.
+  unfold harray.
+  forward.
+  rewrite Znth_map; trivial.
+  entailer!.
+  forward.
+  do 2 (rewrite Znth_map; trivial).
+  entailer!.
+  forward.
+  repeat rewrite Znth_map in *; trivial.
+  entailer!.
+Qed.
+
+Lemma body_size: semax_body Vprog Gprog f_size size_spec.
+Proof.
+  start_function.
+  unfold valid_pq.
+  Intros arr junk.
+  forward.
+  forward.
+  unfold valid_pq.
+  Exists arr. Exists junk.
+  entailer!.
+Qed.
+
+Lemma body_capacity: semax_body Vprog Gprog f_capacity capacity_spec.
+Proof.
+  start_function.
+  unfold valid_pq.
+  Intros arr junk.
+  forward.
+  forward.
+  unfold valid_pq.
+  Exists arr. Exists junk.
+  entailer!.
+Qed.
+
 
 Lemma body_exch: semax_body Vprog Gprog f_exch exch_spec.
 Proof.
