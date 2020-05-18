975b970989eff814a80ec794f73f4b1e68e73cbc
Aquinas <hobor@comp.nus.edu.sg>
2020-05-19 02:31:52 +0800

    Moved internal #defines to C file.  Fixed #defines to use unsigned int consts.
    For header file:
    
    Commented insert/remove as they are not yet verified.  Please use insert_nc
    and remove_nc for now (probably a better idea in any event for the applications
    we are verifying at this point).
    
    Added capacity function.
    
    Removed free_pq, but it's reasonable to add it back at some point.
    
    Once we have a precise bound from body_sink, we can move on to make().  Should
    probably take a capacity rather than the INITIAL_SIZE constant.

--- sample_mark/binary_heap.h
+++ sample_mark/binary_heap.h
@@ -4,11 +4,6 @@
 #define INITIAL_SIZE 8 /* Probably should be a power of 2, no more than max_int / 2 - 1? */
 #define MAX_SIZE (1 << 29) /* for 32-bit machine, is this the best we can do? */
 
-#define ROOT_IDX       0
-#define LEFT_CHILD(x)  (2 * x) + 1
-#define RIGHT_CHILD(x) LEFT_CHILD(x) + 1
-#define PARENT(x)      (x - 1) / 2
-
 typedef struct structItem {
   int priority;
   void* data; /* Should this be a union of void* and int? */
@@ -21,9 +16,18 @@ typedef struct structPQ {
 } PQ;
 
 PQ* make();
-void insert(PQ *pq, Item *x);
-Item* remove_min(PQ *pq);
+
+/*
+ void insert(PQ *pq, Item *x);
+ Item* remove_min(PQ *pq);
+*/
+
+void remove_min_nc(PQ *pq, Item* item);
+void insert_nc(PQ *pq, Item *x);
+
 unsigned int capacity(PQ *pq);
-void free_pq(PQ *pq);
+unsigned int size(PQ *pq);
+
+/* void free_pq(PQ *pq); */
 
 #endif
