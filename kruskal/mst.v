fdc6caaa0ff29316b30ec3c25662ffd67a336444
Anshuman Mohan <anshumanmohan@live.com>
2020-05-22 16:18:49 +0800

    stash

--- kruskal/mst.v
+++ kruskal/mst.v
@@ -10,8 +10,7 @@ Require Import RamifyCoq.graph.FiniteGraph.
 Require Import RamifyCoq.kruskal.undirected_graph.
 
 Context {V E: Type} {EV: EqDec V eq} {EE: EqDec E eq}.
-Context {DV DG: Type}.
-
+Context {DV DE DG: Type}.
 Local Open Scope Z_scope.
 
 Instance Z_EqDec : EqDec Z eq. Proof. hnf. intros. apply Z.eq_dec. Defined.
@@ -22,11 +21,33 @@ Definition is_null_Z: DecidablePred Z := existT (fun P : Z -> Prop => forall a :
 Coercion pg_lg: LabeledGraph >-> PreGraph.
 Coercion lg_gg: GeneralGraph >-> LabeledGraph. 
 
-Definition DEList (g: LabeledGraph V E DV Z DG) {fg: FiniteGraph g} : list Z :=
+Definition DEList
+           {V E}
+           {EV: EqDec V eq} {EE: EqDec E eq}
+           {DV DE DG}
+           (g: LabeledGraph V E DV DE DG)
+           {fg: FiniteGraph g} : list DE :=
   map (elabel g) (EList g).
 
-Definition sum_DE (g: LabeledGraph V E DV Z DG) {fg: FiniteGraph g} : Z :=
-  fold_left Z.add (DEList g) 0.
+Definition sum_DE
+           {V E}
+           {EV: EqDec V eq} {EE: EqDec E eq}
+           {DV DE DG}
+           (g: LabeledGraph V E DV DE DG)
+           (DEadd: DE -> DE -> DE)
+           (DEinit : DE)
+           {fg: FiniteGraph g} : DE :=
+  fold_left DEadd (DEList g) DEinit.
 
-Definition minimum_spanning_forest (g t: LabeledGraph V E DV Z DG) {fg: FiniteGraph g} {ft: FiniteGraph t}:=
-  spanning_uforest g t /\ forall (t': LabeledGraph V E DV Z DG) {ft': FiniteGraph t'}, spanning_uforest g t' -> sum_DE t <= sum_DE t'.
+Definition minimum_spanning_forest
+           {V E EV EE DV DE DG}
+           (g t : LabeledGraph V E DV DE DG)
+           (DEadd: DE -> DE -> DE)
+           (DEinit : DE)
+           (DEComp : DE -> DE -> Prop)
+           {fg: FiniteGraph g}
+           {ft: FiniteGraph t} :=
+  @spanning_uforest V E EV EE g t /\
+  forall (t': LabeledGraph V E DV DE DG) {ft': FiniteGraph t'},
+    @spanning_uforest V E EV EE g t' ->
+    DEComp (sum_DE t DEadd DEinit) (sum_DE t' DEadd DEinit).
