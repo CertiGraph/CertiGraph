ced9a9cdc95d88fc1d322e89607c88458bc443b0
Anshuman Mohan <anshumanmohan@live.com>
2020-07-14 00:02:46 +0800

    fiddles

--- dijkstra/dijkstra_constants.v
+++ dijkstra/dijkstra_constants.v
@@ -1,7 +1,6 @@
-Require Import compcert.lib.Integers.
-Require Import Coq.ZArith.ZArith.
 Require Export RamifyCoq.priq.priq_arr_utils.
-Local Open Scope Z_scope.
 
-(* No need to define the inf and SIZE again. *)
-(* Just peek at the inf and SIZE from PQ *)
+(* 
+ * There is no need to define the inf and SIZE again.
+ * Just peek at the inf and SIZE from PQ. 
+ *)
