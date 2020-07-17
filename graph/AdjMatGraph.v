14204723b063b0944dbcb72b8e84fcc9bcc08b91
Anshuman Mohan <anshumanmohan@live.com>
2020-07-17 16:18:34 +0800

    stash

--- graph/AdjMatGraph.v
+++ graph/AdjMatGraph.v
@@ -9,10 +9,14 @@ Require Import RamifyCoq.lib.Coqlib.
 
 (*
   AdjMat wishlist
-  0. premised with some "size"
+  0. some distinguished inf value
   1. V: Z, E: Z, Elabel: Z
-  2. elabel_inrange: Z -> Pred
-  3. it's "square"
+  2. forall e, elabel_inrange: e -> Pred
+     customizably explains the upper and lower bounds
+  3. forall e, elabel_lt_inf: e -> Pred
+     elabel e < inf
+
+
   4. vert to list function
 
   5. graph to mat function --> elsewhere?
@@ -27,47 +31,28 @@ Require Import RamifyCoq.lib.Coqlib.
 (* strong_evalid *)
 (* evalid --> 0 <= v < size *)
 
-Local Open Scope Z_scope.
-
-(* Specific to Adjacency Matrix representations. *)
-(*
- |  V  |    E    |    DV   |  DE |  DG  | soundness |
- |-----|---------|---------|-----|------|-----------| 
- |  Z  |  Z * Z  | list DE |  Z  | unit |  Finite   | 
- *)
-
-Definition VType : Type := Z.
-Definition EType : Type := Z * Z.
-Definition LE : Type := Z.
-Definition LV: Type := list Z.
-Definition LG: Type := unit.
-
-Instance V_EqDec: EqDec VType eq.
-Proof. hnf. apply Z.eq_dec. Qed.
-
-Instance E_EqDec: EqDec EType eq.
-Proof.
-  hnf. intros [x] [y].
-  destruct (equiv_dec x y).
-  - hnf in e. destruct (Z.eq_dec z z0); subst.
-    + left; reflexivity.
-    + right. intro. apply n. inversion H. reflexivity.
-  - right; intro; apply c; inversion H; reflexivity.
-Defined.
+(* Most of the types are constrained because we want easy
+   AdjMat representation. *)
 
 Instance Z_EqDec : EqDec Z eq. Proof. hnf. intros. apply Z.eq_dec. Defined.
 
-Definition is_null_Z: DecidablePred Z := existT (fun P : Z -> Prop => forall a : Z, {P a} + {~ P a}) (fun x : Z => x < 0) (fun a : Z => Z_lt_dec a 0).
+Definition AdjMatLG := (@LabeledGraph Z (Z*Z) _ _ unit Z unit).
+
+Local Coercion pg_lg: LabeledGraph >-> PreGraph.
+
+(* eventually, this will return:
+   if evalid e then (elabel e) else inf
+ *)
+Definition elabel_w_inf (g: AdjMatLG) (inf: Z) e :=
+  elabel g e.
 
 (* Assumption: (v,0), (v,1) ... (v, size-1) are valid edges.
    What it does: makes a list containing each edge's elabel.
  *)  
-Definition vert_to_list
-           (g: LabeledGraph VType (VType * Z) LV LE LG)
-           (size : Z)
-           (v : VType) : list LE :=
-  map (elabel g) (map (fun x => (v,x)) (nat_inc_list (Z.to_nat size))).
-
+Definition vert_to_list (g: AdjMatLG) (size inf: Z) (v : Z) :=
+  map (elabel_w_inf g inf)
+      (map (fun x => (v,x))
+           (nat_inc_list (Z.to_nat size))).
 
 (* Assumptions: 
    1. 0, 1, ... (size-1) are valid vertices
@@ -79,37 +64,40 @@ Definition vert_to_list
       is a vertex's edge-label-list (see helper above).
  *)
 Definition graph_to_mat_gen
-           (g: LabeledGraph VType EType LV LE LG)
-           (size : Z) : list (list LE) :=
-  map (vert_to_list g size) (nat_inc_list (Z.to_nat size)).
+           (g: AdjMatLG)
+           (size inf : Z) : list (list Z) :=
+  map
+    (vert_to_list g size inf)
+    (nat_inc_list (Z.to_nat size)).
 
 Definition graph_to_list_gen
-           (g: LabeledGraph VType EType LV LE LG)
-           (size : Z) : list LE :=
-  (concat (graph_to_mat_gen g size)).
+           (g: AdjMatLG)
+           (size inf : Z) : list Z :=
+  (concat (graph_to_mat_gen g size inf)).
 
 Class SpaceAdjMatGraph (Addr: Type) (Pred: Type) :=
-  abstract_data_at: Addr -> list LE -> Pred.
+  abstract_data_at: Addr -> list Z -> Pred.
 
 Context {Pred: Type}.
 Context {Addr: Type}.
 Context {SAMG : SpaceAdjMatGraph Addr Pred}.
 
-(* Assumptions: 
-   See above
+(* 
+Assumptions: 
+See above
 
-   What it does: 
-   Flattens the list of lists into one list, and then uses
-    abstract_data_at to create a spatial representation.
+What it does: 
+Uses abstract_data_at to create a spatial representation.
 
-   This is not currently used by SpaceDijkGraph because iter_sepcon 
-   is cleaner. However, just keep it around because it is a general model.
+This is not currently used by SpaceDijkGraph because 
+iter_sepcon is cleaner. However, just keep it around 
+because it is a general model.
 
-   For a (better?) example of this in use for VST, see out
-   dijkstra/SpaceDijkGraph.v
+For a (better?) example of this in use for VST, see out
+dijkstra/SpaceDijkGraph.v
  *)
 Definition AdjMatGraph_rep
-           (g: LabeledGraph VType EType LV LE LG)
-           (size : Z) 
+           (g: AdjMatLG)
+           (size inf : Z) 
            (a : Addr) : Pred :=
-  abstract_data_at a (graph_to_list_gen g size).
+  abstract_data_at a (graph_to_list_gen g size inf).
