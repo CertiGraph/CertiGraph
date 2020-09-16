c3db1bedb2c40952036b79e0ff27bf3681e20008
Anshuman Mohan <anshumanmohan@live.com>
2020-09-16 17:10:57 +0800

    Added the old PQ files back into the development. Prim works again.

---
+++ priq/priq_arr.h
@@ -0,0 +1,11 @@
+#define INT_MAX 2147483647
+#define SIZE 8
+#define IFTY INT_MAX - INT_MAX/SIZE
+
+void push (int vertex, int weight, int pq[SIZE]);
+
+int popMin (int pq[SIZE]);
+
+void adjustWeight (int vertex, int newWeight, int pq[SIZE]);
+
+int pq_emp (int pq[SIZE]);
