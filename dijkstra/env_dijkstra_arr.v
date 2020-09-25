8e950e15d631b70e0cb02e8ff662b142a19a705a
Anshuman Mohan <anshumanmohan@live.com>
2020-09-25 14:10:38 +0800

    Cut down an AdjMat dependency

--- dijkstra/env_dijkstra_arr.v
+++ dijkstra/env_dijkstra_arr.v
@@ -7,7 +7,6 @@ Require Export VST.msl.iter_sepcon.
 Require Export CertiGraph.lib.List_ext.
 Require Export CertiGraph.graph.graph_model.
 Require Export CertiGraph.graph.path_lemmas.
-Require Export CertiGraph.graph.AdjMatGraph_new.
 Require Export CertiGraph.floyd_ext.share.
 
 Definition vertex_type := tint.
