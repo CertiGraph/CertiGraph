715173a39f32e878d7106a12a821423fc653b4ae
Anshuman Mohan <anshumanmohan@live.com>
2020-09-27 16:42:57 +0800

    General cleanup in Dijkstra family

--- dijkstra/dijkstra_spec.v
+++ dijkstra/dijkstra_spec.v
@@ -15,86 +15,86 @@ Local Open Scope Z_scope.
 
 Section DijkstraSpec.
 
-Context {size : Z}.
-Context {inf : Z}.
+  Context {size : Z}.
+  Context {inf : Z}.
 
-Instance CompSpecs : compspecs. Proof. make_compspecs prog. Defined.
-Definition Vprog : varspecs. mk_varspecs prog. Defined.
-Global Existing Instance CompSpecs.
+  Instance CompSpecs : compspecs. Proof. make_compspecs prog. Defined.
+  Definition Vprog : varspecs. mk_varspecs prog. Defined.
+  Global Existing Instance CompSpecs.
 
-Definition getCell_spec :=
-  DECLARE _getCell
-  WITH sh: wshare,
-       g: @DijkGG size inf,
-       graph_ptr: pointer_val,
-       addresses: list val,
-       u: V,
-       i : V
-  PRE [tptr (tptr tint), tint, tint]
-  PROP (0 <= i < size;
-       0 <= u < size)
-  PARAMS (pointer_val_val graph_ptr;
-         Vint (Int.repr u);
-         Vint (Int.repr i))
-  GLOBALS ()
-  SEP (DijkGraph sh CompSpecs g (pointer_val_val graph_ptr) size addresses)
-  POST [tint]
-  PROP ()
-  RETURN (Vint (Int.repr (Znth i (Znth u (@graph_to_mat size g id))))) 
-  SEP (DijkGraph sh CompSpecs g (pointer_val_val graph_ptr) size addresses).    
+  Definition getCell_spec :=
+    DECLARE _getCell
+    WITH sh: wshare,
+         g: @DijkGG size inf,
+         graph_ptr: pointer_val,
+         addresses: list val,
+         u: V,
+         i : V
+    PRE [tptr (tptr tint), tint, tint]
+      PROP (0 <= i < size;
+          0 <= u < size)
+      PARAMS (pointer_val_val graph_ptr;
+           Vint (Int.repr u);
+           Vint (Int.repr i))
+      GLOBALS ()
+      SEP (DijkGraph sh CompSpecs g (pointer_val_val graph_ptr) size addresses)
+    POST [tint]
+      PROP ()
+      RETURN (Vint (Int.repr (Znth i (Znth u (@graph_to_mat size g id))))) 
+      SEP (DijkGraph sh CompSpecs g (pointer_val_val graph_ptr) size addresses).    
+  
+  Definition dijkstra_spec :=
+    DECLARE _dijkstra
+    WITH sh: wshare,
+         g: DijkGG,
+         graph_ptr : pointer_val,
+         addresses : list val,
+         dist_ptr : pointer_val,
+         prev_ptr : pointer_val,
+         src : V
+    PRE [tptr (tptr tint), tint, tptr tint, tptr tint, tint, tint]
+      PROP (0 <= src < size;
+           Forall (fun list => Zlength list = size) (@graph_to_mat size g id))
+      PARAMS (pointer_val_val graph_ptr;
+             Vint (Int.repr src);
+             pointer_val_val dist_ptr;
+             pointer_val_val prev_ptr;
+             Vint (Int.repr size);
+             Vint (Int.repr inf))
+      GLOBALS ()
+      SEP (DijkGraph sh CompSpecs g (pointer_val_val graph_ptr) size addresses;
+          data_at_ Tsh (tarray tint size) (pointer_val_val dist_ptr);
+          data_at_ Tsh (tarray tint size) (pointer_val_val prev_ptr))
+    POST [tvoid]
+      EX prev: list V,
+      EX dist: list V,
+      EX priq: list V,
+      EX priq_ptr: pointer_val,
+      EX popped: list V,                             
+      PROP (forall dst,
+               vvalid g dst ->
+               @inv_popped size inf g src popped prev dist dst)
+      LOCAL ()
+      SEP (DijkGraph sh CompSpecs g (pointer_val_val graph_ptr) size addresses;
+          data_at Tsh (tarray tint size) (map Vint (map Int.repr prev)) (pointer_val_val prev_ptr);
+          data_at Tsh (tarray tint size) (map Vint (map Int.repr dist)) (pointer_val_val dist_ptr);
+          data_at Tsh (tarray tint size) (map Vint (map Int.repr priq)) (pointer_val_val priq_ptr)).
+  
+  Definition mallocN_spec :=
+    DECLARE _mallocN
+    WITH sh:wshare, n: Z
+    PRE [tint]
+      PROP (4 <= n <= Int.max_unsigned)
+      PARAMS (Vint (Int.repr n))
+      GLOBALS ()
+      SEP ()
+    POST [ tptr tvoid ]
+      EX v: pointer_val,
+      PROP (malloc_compatible n (pointer_val_val v))
+      LOCAL (temp ret_temp (pointer_val_val v))
+      SEP (data_at_ Tsh (tarray tint (n / sizeof tint)) (pointer_val_val v)).
 
-Definition dijkstra_spec :=
-  DECLARE _dijkstra
-  WITH sh: wshare,
-       g: DijkGG,
-       graph_ptr : pointer_val,
-       addresses : list val,
-       dist_ptr : pointer_val,
-       prev_ptr : pointer_val,
-       src : V
-  PRE [tptr (tptr tint), tint, tptr tint, tptr tint, tint, tint]
-  PROP (0 <= src < size;
-       Forall (fun list => Zlength list = size) (@graph_to_mat size g id))
-  PARAMS (pointer_val_val graph_ptr;
-         Vint (Int.repr src);
-         pointer_val_val dist_ptr;
-         pointer_val_val prev_ptr;
-         Vint (Int.repr size);
-         Vint (Int.repr inf))
-  GLOBALS ()
-  SEP (DijkGraph sh CompSpecs g (pointer_val_val graph_ptr) size addresses;
-      data_at_ Tsh (tarray tint size) (pointer_val_val dist_ptr);
-      data_at_ Tsh (tarray tint size) (pointer_val_val prev_ptr))
-  POST [tvoid]
-   EX prev: list V,
-   EX dist: list V,
-   EX priq: list V,
-   EX priq_ptr: pointer_val,
-   EX popped: list V,                             
-   PROP (forall dst,
-            vvalid g dst ->
-            @inv_popped size inf g src popped prev dist dst)
-   LOCAL ()
-   SEP (DijkGraph sh CompSpecs g (pointer_val_val graph_ptr) size addresses;
-       data_at Tsh (tarray tint size) (map Vint (map Int.repr prev)) (pointer_val_val prev_ptr);
-       data_at Tsh (tarray tint size) (map Vint (map Int.repr dist)) (pointer_val_val dist_ptr);
-       data_at Tsh (tarray tint size) (map Vint (map Int.repr priq)) (pointer_val_val priq_ptr)).
-
-Definition mallocN_spec :=
-  DECLARE _mallocN
-  WITH sh:wshare, n: Z
-  PRE [tint]
-     PROP (4 <= n <= Int.max_unsigned)
-     PARAMS (Vint (Int.repr n))
-     GLOBALS ()
-     SEP ()
-  POST [ tptr tvoid ]
-     EX v: pointer_val,
-     PROP (malloc_compatible n (pointer_val_val v))
-     LOCAL (temp ret_temp (pointer_val_val v))
-     SEP (data_at_ Tsh (tarray tint (n / sizeof tint)) (pointer_val_val v)).
-
-(*Definition freeN_spec :=
+  (*Definition freeN_spec :=
   DECLARE _freeN
   WITH sh: share, p: pointer_val, n: Z, contents: list Z
     PRE [tptr tvoid]
@@ -106,17 +106,17 @@ Definition mallocN_spec :=
                  (pointer_val_val p))
   POST [tvoid]
     PROP () LOCAL () SEP (emp).
- *)
+   *)
 
-Definition Gprog : funspecs :=
-  ltac:(with_library prog
-                     [push_spec;
-                     pq_emp_spec;
-                     adjustWeight_spec;
-                     popMin_spec;
-                     mallocN_spec;
-                     (* freeN_spec; *)
-                     getCell_spec;
-                     dijkstra_spec]).
+  Definition Gprog : funspecs :=
+    ltac:(with_library prog
+                       [push_spec;
+                       pq_emp_spec;
+                       adjustWeight_spec;
+                       popMin_spec;
+                       mallocN_spec;
+                       (* freeN_spec; *)
+                       getCell_spec;
+                       dijkstra_spec]).
 
 End DijkstraSpec.
--- dijkstra/dijkstra_spec_pure.v
+++ dijkstra/dijkstra_spec_pure.v
@@ -6,96 +6,101 @@ Local Open Scope Z_scope.
 
 Section DijkstraSpecPure.
 
-Context {size : Z}.
-Context {inf : Z}.
+  Context {size : Z}.
+  Context {inf : Z}.
 
-Definition path_correct (g: @DijkGG size inf) (prev dist: list V) src dst p : Prop  :=
-  valid_path g p /\
-  path_ends g p src dst /\
-  path_cost g p < inf /\ 
-  Znth dst dist = path_cost g p /\
-  Forall (fun x => Znth (snd x) prev = fst x) (snd p).
+  Definition path_correct (g: @DijkGG size inf)
+             (prev dist: list V) src dst p : Prop  :=
+    valid_path g p /\
+    path_ends g p src dst /\
+    path_cost g p < inf /\ 
+    Znth dst dist = path_cost g p /\
+    Forall (fun x => Znth (snd x) prev = fst x) (snd p).
 
-Definition path_globally_optimal (g: @DijkGG size inf) src dst p : Prop :=
-  forall p', valid_path g p' ->
-             path_ends g p' src dst ->
-             path_cost g p <= path_cost g p'.
+  Definition path_globally_optimal (g: @DijkGG size inf) src dst p : Prop :=
+    forall p', valid_path g p' ->
+               path_ends g p' src dst ->
+               path_cost g p <= path_cost g p'.
 
-Definition path_in_popped (g: @DijkGG size inf) popped p :=
-  forall step, In_path g step p ->
-               In step popped.
+  Definition path_in_popped (g: @DijkGG size inf) popped p :=
+    forall step, In_path g step p ->
+                 In step popped.
 
-Definition inv_popped (g: DijkGG) src (popped prev dist : list V) dst :=
-  In dst popped ->
-  (Znth dst dist = inf /\ (* if I'm unreachable *)
-   (forall p, (* I'm unreachable via all paths *)
-       valid_path g p ->
-       path_ends g p src dst ->
-       path_cost g p >= inf))
-  \/
-  (exists p, (* else, I'm optimal *)
-      path_correct g prev dist src dst p /\
-      path_in_popped g popped p /\
-      path_globally_optimal g src dst p).
+  Definition inv_popped (g: DijkGG) src (popped prev dist : list V) dst :=
+    In dst popped ->
+    (Znth dst dist = inf /\ (* if I'm unreachable *)
+     (forall p, (* I'm unreachable via all paths *)
+         valid_path g p ->
+         path_ends g p src dst ->
+         path_cost g p >= inf))
+    \/
+    (exists p, (* else, I'm optimal *)
+        path_correct g prev dist src dst p /\
+        path_in_popped g popped p /\
+        path_globally_optimal g src dst p).
 
-Definition inv_unpopped (g : @DijkGG size inf) src (popped prev dist: list V) (dst: V) :=
-  ~ In dst popped ->
-  Znth dst dist < inf ->
-  dst = src \/
-  (dst <> src /\
-   let mom := Znth dst prev in
-   vvalid g mom /\
-   In mom popped /\
-   elabel g (mom, dst) < inf /\
-   Znth mom dist + elabel g (mom, dst) < inf /\
-   Znth dst dist = Znth mom dist + elabel g (mom, dst) /\
-   forall mom',
-     vvalid g mom' ->
-     In mom' popped ->
-     Znth dst dist <= Znth mom' dist + elabel g (mom', dst)).
+  Definition inv_unpopped (g : @DijkGG size inf) src
+             (popped prev dist: list V) (dst: V) :=
+    ~ In dst popped ->
+    Znth dst dist < inf ->
+    dst = src \/
+    (dst <> src /\
+     let mom := Znth dst prev in
+     vvalid g mom /\
+     In mom popped /\
+     elabel g (mom, dst) < inf /\
+     Znth mom dist + elabel g (mom, dst) < inf /\
+     Znth dst dist = Znth mom dist + elabel g (mom, dst) /\
+     forall mom',
+       vvalid g mom' ->
+       In mom' popped ->
+       Znth dst dist <= Znth mom' dist + elabel g (mom', dst)).
 
-Definition inv_unpopped_weak (g : @DijkGG size inf) (src: V) (popped prev dist : list V) (dst u : V) :=
-  ~ In dst popped ->
-  Znth dst dist < inf ->
-  dst = src \/
-  dst <> src /\
-  (let mom := Znth dst prev in
-   mom <> u /\
-   vvalid g mom /\
-   In mom popped /\
-   (elabel g (mom, dst)) < inf /\
-   Znth mom dist + elabel g (mom, dst) < inf /\
-   Znth dst dist = Znth mom dist + (elabel g (mom, dst))) /\
-  forall mom',
-    mom' <> u ->
-    vvalid g mom' ->
-    In mom' popped ->
-    Znth dst dist <= Znth mom' dist + elabel g (mom', dst).
+  Definition inv_unpopped_weak (g : @DijkGG size inf) (src: V)
+             (popped prev dist : list V) (dst u : V) :=
+    ~ In dst popped ->
+    Znth dst dist < inf ->
+    dst = src \/
+    dst <> src /\
+    (let mom := Znth dst prev in
+     mom <> u /\
+     vvalid g mom /\
+     In mom popped /\
+     (elabel g (mom, dst)) < inf /\
+     Znth mom dist + elabel g (mom, dst) < inf /\
+     Znth dst dist = Znth mom dist + (elabel g (mom, dst))) /\
+    forall mom',
+      mom' <> u ->
+      vvalid g mom' ->
+      In mom' popped ->
+      Znth dst dist <= Znth mom' dist + elabel g (mom', dst).
   
-Definition inv_unseen (g : DijkGG) (src: V) (popped prev dist: list V) (dst : V) :=
-  ~ In dst popped ->
-  Znth dst dist = inf ->
-  forall m p2m,
-    vvalid g m ->
-    In m popped ->
-    path_correct g prev dist src m p2m ->
-    path_cost g (path_glue p2m (m, [(m, dst)])) >= inf.
+  Definition inv_unseen (g : DijkGG) (src: V)
+             (popped prev dist: list V) (dst : V) :=
+    ~ In dst popped ->
+    Znth dst dist = inf ->
+    forall m p2m,
+      vvalid g m ->
+      In m popped ->
+      path_correct g prev dist src m p2m ->
+      path_cost g (path_glue p2m (m, [(m, dst)])) >= inf.
 
-Definition inv_unseen_weak (g : DijkGG) (src: V) (popped prev dist: list V) (dst u : V) :=
-  ~ In dst popped ->
-  Znth dst dist = inf ->
-  forall m p2m,
-    vvalid g m ->
-    In m popped ->
-    m <> u ->
-    path_correct g prev dist src m p2m ->
-    path_cost g (path_glue p2m (m, [(m, dst)])) >= inf.
-                                                           
-Definition dijkstra_correct (g : DijkGG) src popped prev dist : Prop :=
-  forall dst,
-    vvalid g dst ->
-    inv_popped g src popped prev dist dst /\
-    inv_unpopped g src popped prev dist dst /\
-    inv_unseen g src popped prev dist dst.
+  Definition inv_unseen_weak (g : DijkGG) (src: V)
+             (popped prev dist: list V) (dst u : V) :=
+    ~ In dst popped ->
+    Znth dst dist = inf ->
+    forall m p2m,
+      vvalid g m ->
+      In m popped ->
+      m <> u ->
+      path_correct g prev dist src m p2m ->
+      path_cost g (path_glue p2m (m, [(m, dst)])) >= inf.
+  
+  Definition dijkstra_correct (g : DijkGG) src popped prev dist : Prop :=
+    forall dst,
+      vvalid g dst ->
+      inv_popped g src popped prev dist dst /\
+      inv_unpopped g src popped prev dist dst /\
+      inv_unseen g src popped prev dist dst.
 
 End DijkstraSpecPure.
