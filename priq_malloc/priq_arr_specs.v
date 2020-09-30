34b53128003fb0dc623c3b98f76edf53ab68c81a
Anshuman Mohan <anshumanmohan@live.com>
2020-09-30 09:13:11 +0800

    PQ is re-verified. It also uses contexts to take size and inf smoothly.

--- priq_malloc/priq_arr_specs.v
+++ priq_malloc/priq_arr_specs.v
@@ -3,10 +3,43 @@ Require Export CertiGraph.priq_malloc.priq_arr_utils.
 Require Export VST.floyd.proofauto.
 
 (* Specs for Anshuman's simple array-based PQ *)
+Section PQSpec.
+
+Context {size : Z}.
+Context {inf : Z}.
+  
+Definition mallocN_spec {CS: compspecs} :=
+  DECLARE _mallocN
+  WITH n: Z
+  PRE [tint]
+  PROP (4 <= n <= Int.max_unsigned)
+  PARAMS (Vint (Int.repr n))
+  GLOBALS ()
+  SEP ()
+  POST [ tptr tvoid ]
+  EX v: pointer_val,
+  PROP (malloc_compatible n (pointer_val_val v))
+  LOCAL (temp ret_temp (pointer_val_val v))
+  SEP (data_at_ Tsh (tarray tint (n / sizeof tint)) (pointer_val_val v)).
+
+Definition init_spec {CS: compspecs} :=
+  DECLARE _init
+  WITH _: unit (* how to take nothing? *)
+  PRE [tint]
+  PROP (Int.min_signed <= size * 4 <= Int.max_signed;
+       0 < size)
+  PARAMS (Vint (Int.repr size))
+  GLOBALS ()
+  SEP ()
+  POST [tptr tint]
+  EX pq: pointer_val,
+  PROP ()
+  LOCAL ()
+  SEP (data_at_ Tsh (tarray tint size) (pointer_val_val pq)).
 
 Definition push_spec {CS: compspecs} :=
   DECLARE _push
-  WITH pq: val, vertex : Z, weight : Z, priq_contents_val: list val, size: Z, inf: Z
+  WITH pq: val, vertex : Z, weight : Z, priq_contents_val: list val
   PRE [tint, tint, tptr tint]
   PROP (0 <= vertex < size;
        weight_inrange_priq weight inf)
@@ -24,7 +57,7 @@ Definition push_spec {CS: compspecs} :=
     
 Definition pq_emp_spec {CS: compspecs} := 
   DECLARE _pq_emp
-  WITH pq: val, priq_contents: list Z, size: Z, inf: Z
+  WITH pq: val, priq_contents: list Z
   PRE [tint, tint, tptr tint]
   PROP (inrange_priq inf priq_contents;
        0 <= size <= Int.max_signed;
@@ -42,7 +75,7 @@ Definition pq_emp_spec {CS: compspecs} :=
 
 Definition adjustWeight_spec {CS: compspecs} :=
   DECLARE _adjustWeight
-  WITH pq: val, vertex : Z, newWeight : Z, priq_contents: list Z, size: Z, inf: Z
+  WITH pq: val, vertex : Z, newWeight : Z, priq_contents: list Z
   PRE [tint, tint, tptr tint]
   PROP (0 <= vertex < size;
        weight_inrange_priq newWeight inf)
@@ -60,7 +93,7 @@ Definition adjustWeight_spec {CS: compspecs} :=
 
 Definition popMin_spec {CS: compspecs} :=
   DECLARE _popMin
-  WITH pq: val, priq_contents: list Z, size: Z, inf: Z
+  WITH pq: val, priq_contents: list Z
   PRE [tint, tint, tptr tint]
    PROP (inrange_priq inf priq_contents;
         isEmpty priq_contents inf = Vzero;
@@ -82,7 +115,11 @@ Definition popMin_spec {CS: compspecs} :=
 
 Definition Gprog {CS: compspecs}: funspecs :=
   ltac:(with_library prog
-                     [push_spec;
+                     [mallocN_spec;
+                     init_spec;
+                     push_spec;
                      pq_emp_spec;
                      adjustWeight_spec;
                      popMin_spec]).
+
+End PQSpec.
