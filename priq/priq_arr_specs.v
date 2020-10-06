c6ca9e9d5b820f0b564df58dfbfee1f751f4a27b
Anshuman Mohan <anshumanmohan@live.com>
2020-10-06 22:15:11 +0800

    Removed inrange_priq from priq_arr_specs. make mst is clean.

--- priq/priq_arr_specs.v
+++ priq/priq_arr_specs.v
@@ -10,6 +10,12 @@ Context {inf : Z}.
 Parameter free_tok : val -> Z -> mpred.
 Context {Z_EqDec : EquivDec.EqDec Z eq}. 
 
+Definition weight_inrange_priq item :=
+  Int.min_signed <= item <= inf.
+
+Definition inrange_priq (priq : list Z) :=
+  Forall (fun x => Int.min_signed <= x <= inf + 1) priq.
+
 Definition mallocN_spec {CS: compspecs} :=
   DECLARE _mallocN
   WITH n: Z
@@ -60,7 +66,7 @@ Definition push_spec {CS: compspecs} :=
   WITH pq: val, vertex : Z, weight : Z, priq_contents_val: list val
   PRE [tint, tint, tptr tint]
   PROP (0 <= vertex < size;
-       (@weight_inrange_priq inf weight))
+       (@weight_inrange_priq weight))
   PARAMS (Vint (Int.repr vertex);
           Vint (Int.repr weight);
           pq)
@@ -77,7 +83,7 @@ Definition pq_emp_spec {CS: compspecs} :=
   DECLARE _pq_emp
   WITH pq: val, priq_contents: list Z
   PRE [tint, tint, tptr tint]
-  PROP (@inrange_priq inf priq_contents;
+  PROP (@inrange_priq priq_contents;
        0 <= size <= Int.max_signed;
        0 <= inf;
        Int.min_signed < inf + 1 <= Int.max_signed)
@@ -96,7 +102,7 @@ Definition adjustWeight_spec {CS: compspecs} :=
   WITH pq: val, vertex : Z, newWeight : Z, priq_contents: list Z
   PRE [tint, tint, tptr tint]
   PROP (0 <= vertex < size;
-       @weight_inrange_priq inf newWeight)
+       @weight_inrange_priq newWeight)
   PARAMS (Vint (Int.repr vertex);
           Vint (Int.repr newWeight);
           pq)
@@ -113,7 +119,7 @@ Definition popMin_spec {CS: compspecs} :=
   DECLARE _popMin
   WITH pq: val, priq_contents: list Z
   PRE [tint, tint, tptr tint]
-   PROP (@inrange_priq inf priq_contents;
+   PROP (@inrange_priq priq_contents;
         @isEmpty inf priq_contents = Vzero;
         0 < size <= Int.max_signed;
         0 <= inf;
