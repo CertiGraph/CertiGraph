97ad2972cae7e4f9ccdb24561c178165eadc9bdb
Anshuman Mohan <anshumanmohan@live.com>
2021-01-30 06:19:19 -0500

    Cleared an admit by adding size bound to overall precon

--- dijkstra/dijkstra_spec1.v
+++ dijkstra/dijkstra_spec1.v
@@ -73,7 +73,8 @@ Section DijkstraSpec.
          src : V
     PRE [tptr (tptr tint), tint, tptr tint, tptr tint, tint, tint]
       PROP (0 <= src < size;
-           Forall (fun list => Zlength list = size) (@graph_to_mat size g id))
+           Forall (fun list => Zlength list = size) (@graph_to_mat size g id);
+           12 * size <= Int.max_unsigned)
       PARAMS (pointer_val_val graph_ptr;
              Vint (Int.repr src);
              pointer_val_val dist_ptr;
