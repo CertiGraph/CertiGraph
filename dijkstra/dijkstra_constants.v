29c6619f3e56b1e294a908f06673db51d91b4c20
Anshuman Mohan <anshumanmohan@live.com>
2021-02-06 23:41:30 -0500

    Dijkstra123 all use PQ pro. The new make phony "make cav", which builds everything we discussed in the paper, builds cleanly.

--- dijkstra/dijkstra_constants.v
+++ dijkstra/dijkstra_constants.v
@@ -1,13 +1,13 @@
 Require Import CertiGraph.dijkstra.dijkstra_env.
 
 Definition size := 8 : Z.
-Definition inf := 1879048192 : Z.
-(* Int.max_signed - Int.max_signed/size *)
+Definition inf := 1879048193 : Z.
+(* Int.max_signed - Int.max_signed/size + 1 *)
 
 Lemma size_eq: size = 8%Z.
 Proof. auto. Qed.
 
-Lemma inf_eq: inf = 1879048192%Z.
+Lemma inf_eq: inf = 1879048193%Z.
 Proof. auto. Qed.
 
 Global Opaque size.
