69cda2d21453a6fbc6ec08e2cd20219086e5ffa9
Anshuman Mohan <anshumanmohan@live.com>
2021-02-21 19:54:55 -0500

    Refined path_cost upper bound in path_correct

--- dijkstra/dijkstra_spec_pure.v
+++ dijkstra/dijkstra_spec_pure.v
@@ -32,7 +32,7 @@ Section DijkstraSpecPure.
        or... p's cost is bounded somehow
      *)
     path_ends g p src dst /\
-    path_cost g p < inf /\ 
+    path_cost g p <= (size-1) * (Int.max_signed / size) /\ 
     Znth dst dist = path_cost g p /\
     Forall (fun (x: E) => Znth (snd x) prev = fst x) (snd p) /\
     acyclic_path g p.
