6fd46ba4be8ea094c8aacf4bf5e8cc097aaaad8d
Aquinas <hobor@comp.nus.edu.sg>
2021-03-05 05:38:44 +0800

    A new set of bounds to try.

--- dijkstra/MathDijkGraph.v
+++ dijkstra/MathDijkGraph.v
@@ -13,45 +13,37 @@ Section MathDijkGraph.
 
   (* Show our planned restrictions aren't too restrictive. *)
   Lemma edges_can_be_positive:
-    0 < size <= Int.max_signed ->
-    0 < Int.max_signed / size.
+    0 < size <= Int.max_signed - 1 ->
+    0 < (Int.max_signed - 1) / size.
   Proof. apply Z.div_str_pos. Qed.
 
   Lemma always_room_for_inf:
-    0 < size <= Int.max_signed ->
-    0 <= (Int.max_signed / size) * (size - 1) < Int.max_signed.
+    0 < size <= Int.max_signed - 1 ->
+    0 <= ((Int.max_signed - 1) / size) * size < Int.max_signed.
   Proof.
-    intros. split.
+    intros. forget Int.max_signed as m.
+    split. apply Z.mul_nonneg_nonneg.
+    apply Z.div_pos. 1,2,3: lia.
+    pose proof (Z.mul_div_le (m - 1) size). spec H0. lia.
+    apply Z.le_lt_trans with (m-1). 2: lia. lia.
+(*    intros. split.
     apply Z.mul_nonneg_nonneg. apply Z.div_pos. 1,2,3: lia.
     rewrite Z.mul_sub_distr_l, Z.mul_1_r.
     apply Z.le_lt_trans with (Int.max_signed - Int.max_signed / size).
     pose proof (Z.mul_div_le Int.max_signed size). lia.
     pose proof edges_can_be_positive. lia.
+*)
   Qed.
 
   Lemma edges_can_be_positive':
     0 < size * 4 <= Int.max_signed ->
-    0 < Int.max_signed / size.
+    0 < (Int.max_signed - 1) / size.
   Proof. intros. apply edges_can_be_positive. lia. Qed.
 
   Lemma always_room_for_inf':
     0 < size * 4 <= Int.max_signed ->
-    0 <= (Int.max_signed / size) * (size - 1) < Int.max_signed.
+    0 <= ((Int.max_signed - 1) / size) * size < Int.max_signed.
   Proof. intros. apply always_room_for_inf. lia. Qed.
-
-(* Another bound we've experimented with...
-
-  Lemma bound_has_room_for_inf: forall size',
-    0 < size' <= Int.max_signed ->
-    0 <= ((Int.max_signed - 1) / size') * size' < Int.max_signed.
-  Proof.
-    intros. forget Int.max_signed as m.
-    split. apply Z.mul_nonneg_nonneg.
-    apply Z.div_pos. 1,2,3: lia.
-    pose proof (Z.mul_div_le (m - 1) size'). spec H0. lia.
-    apply Z.le_lt_trans with (m-1). 2: lia. lia.
-  Qed.
-*)
 
   (* Here is the LabeledGraph *)
   Definition DijkLG := AdjMatLG.
@@ -62,7 +54,7 @@ Section MathDijkGraph.
     basic:
       (* first, we can take AdjMat's soundness wholesale *)
       @SoundAdjMat size inf g;
-    
+
     veb:
       (* from the AdjMat soundness above we already know 
          e is representable, 
@@ -70,15 +62,15 @@ Section MathDijkGraph.
        *)
       forall e,
         evalid g e ->
-        0 <= elabel g e <= Int.max_signed / size;
+        0 <= elabel g e <= (Int.max_signed - 1) / size;
 
     sfr: (* size is further restricted *)
       size * 4 <= Int.max_signed;
     (* because sizeof tint = 4 *)
-    
+
     ifr: (* inf is further restricted *)
-      (Int.max_signed / size) * (size - 1) < inf
-                                                        
+      ((Int.max_signed - 1) / size) * size < inf
+
     }.
 
   (* And here is the GeneralGraph that we will use *)
@@ -170,12 +162,13 @@ Section MathDijkGraph.
     destruct (@evalid_dec _ _ _ _ g (finGraph g) e).
     - specialize (H e0). 
       split; trivial. rep_lia.
-      apply Z.le_trans with (m := (Int.max_signed / size)).
+      apply Z.le_trans with (m := ((Int.max_signed - 1) / size)).
       apply H.
       pose proof (size_representable g).
-      apply Z.le_trans with (m := Int.max_signed / size).
+      apply Z.le_trans with (m := (Int.max_signed - 1) / size).
       lia.
-      apply div_pos_le; lia.
+      etransitivity.
+      apply div_pos_le; lia. lia.
     - rewrite H0 in n.
       replace (elabel g e) with inf by trivial.
       pose proof (inf_representable g).
@@ -200,19 +193,16 @@ Section MathDijkGraph.
 
   Lemma inf_gt_largest_edge:
     forall (g: DijkGG),
-    1 < size -> (* experimental *)
-    Int.max_signed / size < inf.
+    (Int.max_signed - 1) / size < inf.
   Proof.
     intros.
     pose proof (inf_further_restricted g).
     pose proof (size_representable g).
-    apply Z.le_lt_trans with (m := Int.max_signed / size * (size - 1)); trivial.
-    rewrite <- (Z.mul_1_r (Int.max_signed / size)) at 1.
-    apply Z.mul_le_mono_nonneg_l. 2: lia.
-    apply Z.div_pos; lia.
-(*
+    apply Z.le_lt_trans with (m := ((Int.max_signed - 1) / size) * size); trivial.
+
     apply Z.le_mul_diag_r; [|lia].
-    apply Z.div_str_pos; trivial. *)
+    apply Z.div_str_pos; trivial.
+    destruct g. destruct sound_gg. lia.
   Qed.
 
 End MathDijkGraph.
