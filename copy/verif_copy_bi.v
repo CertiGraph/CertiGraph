9fd15582f10e4bcf27f354379981612c435f9acd
Shengyi Wang <txyyss@gmail.com>
2021-09-28 01:10:01 +0800

    Update verif_copy_bi.v

--- copy/verif_copy_bi.v
+++ copy/verif_copy_bi.v
@@ -136,7 +136,8 @@ Proof.
     1: split.
     + eapply (copy_vgamma_not_null_refl g); eauto.
       clear - H0.
-      destruct d; [change null with (NullPointer) | simpl in H0]; try congruence.
+      destruct d; [change null with (NullPointer) |
+                    unfold nullval in H0; simpl in H0]; try congruence.
     + right.
       inversion H_GAMMA_g; auto.
     + rewrite (va_reachable_invalid _ d); auto.
